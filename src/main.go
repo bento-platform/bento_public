2bede6fe2893cfc202ff776b9500fd1cd8952737
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-12-15 18:06:22 -0500

    echo migration

--- src/main.go
+++ src/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type BentoConfig struct {
@@ -25,23 +26,62 @@ func main() {
 		Katsu URL: %v
 	`, cfg.KatsuUrl))
 
-	// TODO: migrate to using Echo
-	myhttp := http.NewServeMux()
-	fs := http.FileServer(http.Dir("./www/"))
+	// Begin Echo
 
-	myhttp.Handle("/", http.StripPrefix("", fs))
-	myhttp.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
-		// return dummy data
-		// TODO : formalize data structure
-		fmt.Fprintf(w, `[
-			{"id": 0, "sampleId": "HG0100", "age": 45},
-			{"id": 1, "sampleId": "HG0101", "age": 60},
-			{"id": 2, "sampleId": "HG0102", "age": 20}
-		]`)
+	// Instantiate Server
+	e := echo.New()
+
+	// Service Connections:
+	// -- TODO: Katsu Service?
+
+	// Configure Server
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	// -- Override handlers with "custom Gohan" context
+	//		to be able to provide variables and global singletons
+	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// cc := &contexts.PublicContext{c, es, &cfg, *iz}
+			// return h(cc)
+			return h(c)
+		}
 	})
 
-	// TODO: parameterize port
-	port := "8090"
-	log.Println("http://localhost:" + port)
-	http.ListenAndServe(":"+port, myhttp)
+	// Global Middleware
+	e.Use(middleware.Logger())
+
+	// Begin MVC Routes
+	// -- Root : static files
+	e.Use(middleware.Static("./www"))
+
+	// -- Data
+	// TODO: Katsu integration
+	e.GET("/data", func(c echo.Context) error {
+		data := []map[string]interface{}{
+			{
+				"id":       0,
+				"sampleId": "HG0100",
+				"age":      45,
+			},
+			{
+				"id":       1,
+				"sampleId": "HG0101",
+				"age":      60,
+			},
+			{
+				"id":       2,
+				"sampleId": "HG0102",
+				"age":      20,
+			},
+		}
+
+		return c.JSON(http.StatusOK, data)
+	})
+
+	// Run
+	e.Logger.Fatal(e.Start(":8090"))
 }
