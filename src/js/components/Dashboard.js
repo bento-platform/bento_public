c3c6a1e1626cf5041468c42d88f190da81e9772b
brouillette <brennan.brouillette@computationalgenomics.ca>
2022-01-19 18:29:19 -0500

    better dev dashboard

--- src/js/components/Dashboard.js
+++ src/js/components/Dashboard.js
@@ -25,11 +25,39 @@ class Dashboard extends React.Component {
 
     render() {
         const { phenopackets, overview } = this.props;
-        
+        // TODO: refactor
+        //          - simple overview data presentation PoC
+        const experimentTypeData = [];
+        const experimentLibrarySourceData = [];
+        if (typeof overview != undefined && Object.keys(overview).length > 0) {
+            // experiments
+            var experiments = overview.data_type_specific.experiments;
+            
+            // get types
+            var experimentTypes = experiments.experiment_type;
+            var keys = Object.keys(experimentTypes);
+            var values = Object.values(experimentTypes);
+            for (var i = 0; i < keys.length; i++) {
+                experimentTypeData.push({x: keys[i], y: values[i]})
+            }
+
+            // get library sources
+            var experimentLibrarySources = experiments.library_source;
+            var keys = Object.keys(experimentLibrarySources);
+            var values = Object.values(experimentLibrarySources);
+            for (var i = 0; i < keys.length; i++) {
+                experimentLibrarySourceData.push({x: keys[i], y: values[i]})
+            }
+        }
+        // --
+
         return (
             <Container>
                 <Row>
                     <Header/>
+                </Row>
+                <Row>
+                    <span>Overview :</span>
                 </Row>
                 <Row>
                     <Col className="text-center" xs={{ span: 4, offset: 4 }} md={{ span: 6, offset: 3 }}>
@@ -37,31 +65,56 @@ class Dashboard extends React.Component {
                     </Col>
                 </Row>
                 <Row>
-                    <span>Phenopackets :</span>
-                    <Col md={{ span: 8, offset: 2 }}>
+                    <Col md={{ span: 8 }}>
+                        <Row>
+                            <span>Experiments</span>
+                        </Row>
+                        <Row>
                         {
-                            // verify 'phenopackets'
-                            typeof phenopackets == undefined || phenopackets.length == 0 
-                            ? 
+                            // verify 'experimentTypeData'
+                            typeof experimentTypeData == undefined || experimentTypeData.length === 0 
+                            ? // display nothing if there is no data
                             <span>Nothing here...</span> 
                             :
-                            <VictoryPie
-                                data={phenopackets}
-                                // data accessor for x values
-                                x="age"
-                                // data accessor for y values
-                                y="age" />
+                            <div>
+                                <span>Types</span>
+                                <VictoryPie
+                                    width={800}
+                                    data={experimentTypeData}
+                                    // data accessor for x values
+                                    x="x"
+                                    // data accessor for y values
+                                    y="y" />
+                            </div>
                         }
+                        </Row>
+                        <Row>
+                        {
+                            // verify 'experimentLibrarySourceData'
+                            typeof experimentLibrarySourceData == undefined || experimentLibrarySourceData.length === 0 
+                            ? // display nothing if there is no data
+                            <></> 
+                            :
+                            <div>
+                                <span>Library Sources</span>
+                                <VictoryPie
+                                    width={800}
+                                    data={experimentLibrarySourceData}
+                                    // data accessor for x values
+                                    x="x"
+                                    // data accessor for y values
+                                    y="y" />
+                            </div>
+                        }
+                        </Row>
                     </Col>
-                </Row>
-                <Row>
-                    <span>Overview :</span>
-                    <Col md={{ span: 8, offset: 2 }}>
+                    <Col md={{ span: 4 }}>
+                        <Row>
                         {
                             // verify 'overview'
                             typeof overview == undefined || Object.keys(overview).length === 0 
                             ? // display message if there is no data
-                            <span>Nothing here...</span> 
+                            <></> 
                             : // display the available data 
                             <JsonFormatter 
                                 json={JSON.stringify(overview)} 
@@ -72,6 +125,7 @@ class Dashboard extends React.Component {
                                     numberStyle: { color: 'darkorange' }
                                 }} />
                         }
+                        </Row>
                     </Col>
                 </Row>
             </Container>
