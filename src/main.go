4074fcfcef2f9dcf625782b70e534289aac97ff8
brouillette <brennan.brouillette@computationalgenomics.ca>
2022-01-19 16:37:39 -0500

    added dummy public katsu overview endpoint

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -59,7 +61,7 @@ func main() {
 	e.Use(middleware.Static("./www"))
 
 	// -- Data
-	// TODO: Katsu integration
+	// TODO: Remove dummy data
 	e.GET("/data", func(c echo.Context) error {
 		data := []map[string]interface{}{
 			{
@@ -80,6 +82,31 @@ func main() {
 		}
 
 		return c.JSON(http.StatusOK, data)
+	})
+
+	// Katsu testing
+	e.GET("/katsu", func(c echo.Context) error {
+		// Fetch Overview
+		// (TODO: refactor to use Katsu's "public" overview, rather than the "private" one)
+		resp, err := http.Get(fmt.Sprintf("%s/api/overview", cfg.KatsuUrl))
+		if err != nil {
+			fmt.Println(err)
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+
+		defer resp.Body.Close()
+
+		// Read response body and conver to a generic JSON-like datastructure
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+
+		var jsonLike map[string]interface{}
+		json.Unmarshal(body, &jsonLike)
+
+		return c.JSON(http.StatusOK, jsonLike)
 	})
 
 	// Run
