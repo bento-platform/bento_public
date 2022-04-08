927bd91af0bb5145e53cdc58ea9809a7df2fe6c9
Gordon Krieger <gordon.krieger@gmail.com>
2022-04-08 16:39:12 -0400

    set default tab in reducer

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -11,7 +11,7 @@ const INITIAL_STATE = {
 	queryParameterCheckedStack: [],
 	queryResponseData: {},
 	isFetchingData: false,
-	activeTab: ""
+	activeTab: "Overview"
 }
 
 export default (state = INITIAL_STATE, action={}) => {
