d7860cd53401b4b71614cd7aa8f63bd1ea298a1f
Gordon Krieger <gordon.krieger@gmail.com>
2022-04-26 10:32:44 -0400

    formatting for search results

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -2,9 +2,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Button, Container } from 'react-bootstrap';
-import { JsonFormatter } from 'react-json-formatter'
 import { Row, Col } from 'react-bootstrap'
-import Spinner from 'react-bootstrap/Spinner'
+import SearchResults from "./SearchResults";
 
 // do not remove, needed even though not referenced here
 import QueryParameter from './QueryParameter'
@@ -35,45 +34,33 @@ class Search extends React.Component {
             queryParameterStack } = this.props;
 
         return (
-            <Container>
-                <Row>
-                    <Col sm={{ span: 10 }}>
-                    {
-                        // verify 'queryParameterStack'
-                        typeof queryParameterStack == undefined || queryParameterStack.length === 0 
-                        ? // display nothing if there is no data
-                        <span>Nothing here...</span> 
-                        :
-                        <div>
-                            {queryParameterStack}
-                        </div>
-                    }
-                    </Col>
-                    <Col md={{ span: 2 }}>
-                        {
-                            // verify 'queryResponseData'
-                            typeof queryResponseData == undefined || Object.keys(queryResponseData).length === 0 
-                            ? // display message if there is no data
-                            <></> 
-                            : // display the available data 
-                            <JsonFormatter 
-                                json={JSON.stringify(queryResponseData)} 
-                                tabWith='4' 
-                                JsonStyle={{
-                                    propertyStyle: { color: 'red' },
-                                    stringStyle: { color: 'green' },
-                                    numberStyle: { color: 'darkorange' }
-                                }} />
-                        }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="text-center" xs={{ span: 4, offset: 4 }} md={{ span: 6, offset: 3 }}>
-                        <Button variant="primary" onClick={() => this.queryKatsuPublic()} disabled={isFetchingData}>Get Data</Button>
-                        <Spinner animation="border" hidden={!isFetchingData}/>
-                    </Col>
-                </Row>
-            </Container>
+          <Container>
+            <Row>
+              <Col sm={{ span: 10 }}>
+                {
+                  // verify 'queryParameterStack'
+                  typeof queryParameterStack == undefined || queryParameterStack.length === 0 ? (
+                    // display nothing if there is no data
+                    <span>Nothing here...</span>
+                  ) : (
+                    <div>{queryParameterStack}</div>
+                  )
+                }
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center" xs={{ span: 4, offset: 4 }} md={{ span: 6, offset: 3 }}>
+                <Button variant="primary" onClick={() => this.queryKatsuPublic()} disabled={isFetchingData}>
+                  Get Data
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <SearchResults queryResponseData={queryResponseData} isFetchingData={isFetchingData} />
+              </Col>
+            </Row>
+          </Container>
         );
 	}
 }
