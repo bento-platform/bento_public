bb53073ec4df0c8d5d129b288ba405088e86164d
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-18 12:41:55 -0400

    added config for webpack watch and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,56 @@
-const webpack = require('webpack'); // only add this if you don't have yet
-const path = require('path');
+const webpack = require("webpack") // only add this if you don't have yet
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
 
 // load client.env
-require('dotenv').config({ path: './client.env' }); 
-
+require("dotenv").config({ path: "./client.env" })
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.(sass|less|css)$/,
-                loaders: ['style-loader', 'css-loader', 'less-loader']
-            }
-        ]
-    },
-    entry: {
-        js: ['babel-polyfill', './src/js'],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        use: {
+          loader: "babel-loader",
+        },
       },
-    output: {
-        path: __dirname + '/build/www',
-        publicPath: '',
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: './distgetuk'
-    },
-    plugins: [
-        //new Dotenv()
-        new webpack.DefinePlugin({
-            "process.env": JSON.stringify(process.env)
-        }),
-    ]
-};
+      {
+        test: /\.(sass|less|css)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "less-loader" },
+        ],
+      },
+    ],
+  },
+  entry: {
+    js: ["babel-polyfill", "./src/js"],
+  },
+  output: {
+    path: __dirname + "/build/www",
+    publicPath: "",
+    // filename: "js/bundle.js",
+    filename: "js/[name][chunkhash].js",
+  },
+  devServer: {
+    static: "./dist",
+    contentBase: "./distgetuk",
+  },
+  plugins: [
+    //new Dotenv()
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      inject: false,
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+}
