50ea4181ce634229c7ea0feff7f90de05a1a2db5
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-08-09 12:35:18 -0400

    changed to arrow function

--- src/js/utils/string.js
+++ src/js/utils/string.js
@@ -1,5 +1,2 @@
-export const toTitleCase = (str) => {
-  return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
-  });
-};
+export const toTitleCase = (str) =>
+  str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase());
