5af61333ac2147334ce5e3c354c681160610b428
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-09-02 12:39:33 -0400

    precompiled regex

--- src/js/components/Util/LinkIfUrl.js
+++ src/js/components/Util/LinkIfUrl.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Typography } from 'antd';
 
+/*
+^                               starts with...
+    (
+        http|https              "http" or "https"
+    )
+:                               :
+\/\/                            //
+    [^                          anything excluding...
+         "                      space or double quote
+    ]
+    +                           repeated at least once
+$                               until the end of the string
+*/
+const url_regex = /^(http|https):\/\/[^ "]+$/;
+
 // Renders text as link if the text provided is a valid url.
 const LinkIfUrl = ({ text }) => {
-  if (text.match(/^(http|https):\/\/[^ "]+$/)) {
+  if (text.match(url_regex)) {
     return (
       <Typography.Link href={text} target="_blank">
         {text}
