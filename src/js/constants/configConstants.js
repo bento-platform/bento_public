668e09dc9763d541b0903d32204ed4fd6e508987
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-09-07 14:21:34 -0400

    removed unused variable

--- src/js/constants/configConstants.js
+++ src/js/constants/configConstants.js
@@ -1,8 +1,7 @@
-const { CLIENT, HOST, DEBUG, MAX_QUERY_PARAMETERS } = process.env;
+const { CLIENT, HOST, DEBUG } = process.env;
 
 export const debug = DEBUG.toLowerCase() === 'true';
 export const client = CLIENT;
-export const maxQueryParameters = MAX_QUERY_PARAMETERS;
 
 export const configUrl = HOST + '/config';
 export const publicOverviewUrl = HOST + '/overview';
