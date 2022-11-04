3a622e303fc287bc4383f325eb04578fc1c5e10c
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-11-04 12:12:57 -0400

    added tooltip icon to chart card

--- src/js/components/Overview/MakeChartCard.js
+++ src/js/components/Overview/MakeChartCard.js
@@ -12,12 +12,11 @@ const MakeChartCard = ({ section, chart, onRemoveChart }) => {
   const { name, title, data, chartType, config, id, description } = chart;
 
   const extraOptionsData = [
-    // to enable extra buttons, follow the commented example
-    // {
-    //   icon: <UpOutlined />,
-    //   description: 'Move the chart up',
-    //   onClick: () => onMoveChartUp(chart.name),
-    // },
+    {
+      icon: <QuestionOutlined />,
+      description: description,
+      onClick: () => {},
+    },
     {
       icon: <CloseOutlined />,
       description: 'Remove this chart',
@@ -50,12 +49,7 @@ const MakeChartCard = ({ section, chart, onRemoveChart }) => {
 
   return (
     <div key={name} style={{ height: '100%', width: '430px' }}>
-      <Card
-        title={<Tooltip title={description}>{t(title)}</Tooltip>}
-        style={CARD_STYLE}
-        size="small"
-        extra={<Space size="small">{ed}</Space>}
-      >
+      <Card title={t(title)} style={CARD_STYLE} size="small" extra={<Space size="small">{ed}</Space>}>
         <Chart chartType={chartType} data={data} units={config?.units || undefined} />
       </Card>
     </div>
