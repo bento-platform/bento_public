ffd5772b7a6fbadab5d7f23aa07c808f74a29e67
David Lougheed <david.lougheed@gmail.com>
2022-12-16 11:49:11 -0500

    feat: runtime-overwritable about.html

--- src/js/store.js
+++ src/js/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import configReducer from './features/config/config';
+import contentReducer from './features/content/content';
 import dataReducer from './features/data/data';
 import queryReducer from './features/search/query';
 import provenanceReducer from './features/provenance/provenance';
@@ -7,6 +9,7 @@ import provenanceReducer from './features/provenance/provenance';
 export const store = configureStore({
   reducer: {
     config: configReducer,
+    content: contentReducer,
     data: dataReducer,
     query: queryReducer,
     provenance: provenanceReducer,
