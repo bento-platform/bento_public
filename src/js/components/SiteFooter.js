11a02aa8cbfcfac1b8c58b0165c9bf728f4bd971
David Lougheed <david.lougheed@gmail.com>
2022-12-16 10:55:33 -0500

    style: tweak / space out footer a bit

--- src/js/components/SiteFooter.js
+++ src/js/components/SiteFooter.js
@@ -9,34 +9,39 @@ const SiteFooter = () => {
   return (
     <Footer>
       <Row justify="center">
-        <Space size={0} direction="vertical" align="center">
+        <Space size={8} direction="vertical" align="center">
           <div>
             <Title level={5} type="secondary">
               Powered by
             </Title>
             <img style={{ height: '25px' }} src={bentoLogo} alt="Bento" />
           </div>
-          <Text type="secondary">
-            Copyright &copy; 2019-2022 the{' '}
-            <Link href="http://computationalgenomics.ca" target="_blank">
-              Canadian Centre for Computational Genomics
+          <div>
+            <Text type="secondary">
+              Copyright &copy; 2019-2022 the{' '}
+              <Link href="http://computationalgenomics.ca" target="_blank">
+                Canadian Centre for Computational Genomics
+              </Link>
+              .
+            </Text>
+            <br />
+            <Text type="secondary">
+              Bento is licensed under the{' '}
+              <Link href="https://github.com/bento-platform/bento_public/blob/main/LICENSE" target="_blank">
+                LGPLv3
+              </Link>
+              . The source code is available on{' '}
+              <Link href="https://github.com/bento-platform" target="_blank">
+                Github
+              </Link>
+              .
+            </Text>
+          </div>
+          <div>
+            <Link href="/public/terms.html" target="_blank">
+              Terms of Use
             </Link>
-            .
-          </Text>
-          <Text type="secondary">
-            Bento is licensed under the{' '}
-            <Link href="https://github.com/bento-platform/bento_public/blob/main/LICENSE" target="_blank">
-              LGPLv3
-            </Link>
-            . The source code is available on{' '}
-            <Link href="https://github.com/bento-platform" target="_blank">
-              Github
-            </Link>
-            .
-          </Text>
-          <Link href="/public/terms.html" target="_blank">
-            Terms of Use
-          </Link>
+          </div>
         </Space>
       </Row>
     </Footer>
