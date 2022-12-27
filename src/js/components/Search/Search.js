ea35d03b69f56fc15665ee1f1fbd0f07725aa052
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-12-27 17:26:30 -0500

    Enforced vertical display (removed wrap)

--- src/js/components/Search/Search.js
+++ src/js/components/Search/Search.js
@@ -25,18 +25,16 @@ const Search = () => {
   return (
     <>
       <Row justify="center">
-        <Space direction="vertical">
-          <Row justify="center">
-            <SearchResults />
-            <Space direction="vertical" size="large">
-              {searchSections.map((e, i) => (
-                <div key={i}>
-                  <Typography.Title level={4}>{t(e.section_title)}</Typography.Title>
-                  <SearchFieldsStack key={i} queryFields={e.fields} />
-                </div>
-              ))}
-            </Space>
-          </Row>
+        <Space direction="vertical" align="center">
+          <SearchResults />
+          <Space direction="vertical" size="large">
+            {searchSections.map((e, i) => (
+              <div key={i}>
+                <Typography.Title level={4}>{t(e.section_title)}</Typography.Title>
+                <SearchFieldsStack key={i} queryFields={e.fields} />
+              </div>
+            ))}
+          </Space>
         </Space>
       </Row>
       <Button
