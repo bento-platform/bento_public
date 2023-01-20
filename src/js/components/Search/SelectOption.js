e0f6a0bee3cea8fc0cb90f1e112632c55eeb5e2a
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-01-20 13:38:59 -0500

    patch: check undefined truthy form

--- src/js/components/Search/SelectOption.js
+++ src/js/components/Search/SelectOption.js
@@ -24,7 +24,7 @@ const SelectOption = ({ id, isChecked, options, optionalDispatch}) => {
       return;
     }
     // --
-    if (optionalDispatch != undefined) {
+    if (optionalDispatch) {
       dispatch(optionalDispatch());
     }
     // --
