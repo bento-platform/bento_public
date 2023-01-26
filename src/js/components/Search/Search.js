034b9073ad66c42c409b5761210b7621b85f48a0
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-01-26 16:20:02 -0500

    modified back to top

--- src/js/components/Search/Search.js
+++ src/js/components/Search/Search.js
@@ -1,33 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import { Row, Button, Typography, Space } from 'antd';
-import { UpOutlined } from '@ant-design/icons';
-import { useSelector,useDispatch  } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Row, Typography, Space, FloatButton } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-
 
 import SearchFieldsStack from './SearchFieldsStack';
 import SearchResults from './SearchResults';
 
 import { makeGetKatsuPublic } from '../../features/search/query';
 
-const scrollToTop = () => {
-  window.scrollTo(0, 0);
-};
-
 const Search = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
   const searchSections = useSelector((state) => state.query.querySections);
-  
-  const [atTop, setAtTop] = useState(true);
-
-  useEffect(() => {
-    window.onscroll = () => {
-      window.pageYOffset > 0 ? setAtTop(false) : setAtTop(true);
-    }
-
-    return () => (window.onscroll = null);
-  });
 
   useEffect(() => {
     dispatch(makeGetKatsuPublic());
@@ -48,17 +32,7 @@ const Search = () => {
           </Space>
         </Space>
       </Row>
-      <Button
-        type="primary"
-        onClick={scrollToTop}
-        size="large"
-        shape="round"
-        className="floating-search"
-        icon={<UpOutlined />}
-        disabled={atTop}
-      >
-        {t('Back to top')}
-      </Button>
+      <FloatButton.BackTop />
     </>
   );
 };
