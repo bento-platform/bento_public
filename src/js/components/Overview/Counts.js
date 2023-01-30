0fc4602a6b0c0ca1230efba78ba82523448bfc1b
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-01-30 11:07:01 -0500

    updated experiments logo and count color

--- src/js/components/Overview/Counts.js
+++ src/js/components/Overview/Counts.js
@@ -3,8 +3,9 @@ import { useSelector } from 'react-redux';
 import { Typography, Card, Space, Statistic } from 'antd';
 import { TeamOutlined } from '@ant-design/icons';
 import { BiDna } from 'react-icons/bi';
-import { AiOutlineExperiment } from 'react-icons/ai';
 import { useTranslation } from 'react-i18next';
+
+import ExpSvg from '../Util/ExpSvg';
 
 const Counts = () => {
   const { t } = useTranslation();
@@ -22,7 +23,7 @@ const Counts = () => {
     },
     {
       title: 'Experiments',
-      icon: <AiOutlineExperiment />,
+      icon: <ExpSvg />,
       count: useSelector((state) => state.data.counts.experiments),
     },
   ];
@@ -33,7 +34,7 @@ const Counts = () => {
       <Space direction="horizontal">
         {data.map(({ title, icon, count }, i) => (
           <Card key={i}>
-            <Statistic title={t(title)} value={count} valueStyle={{ color: '#1890ff' }} prefix={icon} />
+            <Statistic title={t(title)} value={count} valueStyle={{ color: '#75787a' }} prefix={icon} />
           </Card>
         ))}
       </Space>
