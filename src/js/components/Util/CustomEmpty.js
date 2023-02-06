8fff0ebeb83f50d50b87743ac311e5c7ed4d98e0
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-02-06 13:58:43 -0500

    modified translation ns - Util

--- src/js/components/Util/CustomEmpty.js
+++ src/js/components/Util/CustomEmpty.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Empty } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { DEFAULT_TRANSLATION } from '../../constants/configConstants';
 
 const CustomEmpty = ({ text }) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation(DEFAULT_TRANSLATION);
 
   return <Empty description={t(text)} />;
 };
