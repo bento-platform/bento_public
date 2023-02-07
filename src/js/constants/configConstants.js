4f7921ee4600f1a24ee5a06354c04029afb6011a
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-02-07 11:18:58 -0500

    removed unused const

--- src/js/constants/configConstants.js
+++ src/js/constants/configConstants.js
@@ -6,9 +6,6 @@ export const searchFieldsUrl = '/fields';
 export const katsuUrl = '/katsu';
 export const provenanceUrl = '/provenance';
 
-export const translationEnUrl = '/public/locales/en/translation_en.json';
-export const translationFrUrl = '/public/locales/fr/translation_fr.json';
-
 export const DEFAULT_TRANSLATION = 'default_translation';
 export const NON_DEFAULT_TRANSLATION = 'translation';
 export const ANY_TRANSLATION = [DEFAULT_TRANSLATION, NON_DEFAULT_TRANSLATION];
