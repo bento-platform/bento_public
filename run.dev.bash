1497b1da2ff1bb63a06c7bea08ff65283b58e5dc
David Lougheed <david.lougheed@gmail.com>
2023-02-24 14:25:45 -0500

    fix: try to add a delay to nodemon to fix port issues

--- run.dev.bash
+++ run.dev.bash
@@ -8,7 +8,8 @@ ls -la
 npm install
 
 # Run nodemon as a watcher to recompile JS + Go
-npx nodemon --exec go run main.go --signal SIGTERM &
+#  - add 500ms delay to try to prevent port binding issues
+npx nodemon --exec go run main.go --signal SIGTERM --delay 500ms &
 
 # Run webpack watch to recompile JS files
 npm run watch &
