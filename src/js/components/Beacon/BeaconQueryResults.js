1727238480afdffec1b96fe355cc2e8aa34a04ab
Gordon Krieger <gordon.krieger@gmail.com>
2023-03-29 14:32:30 -0400

    change beacon results box to match style elsewhere

--- src/js/components/Beacon/BeaconQueryResults.js
+++ src/js/components/Beacon/BeaconQueryResults.js
@@ -1,33 +1,31 @@
-import React from "react";
-import { Card, Statistic, Icon } from "antd";
+import React from 'react';
+import { Card, Col, Row, Statistic, Typography, Space } from 'antd';
 import { TeamOutlined } from '@ant-design/icons';
 
+// TODO: change results icon according to which entity requested
 
-// TODO: change results icon according to which entity requested 
+const BeaconQueryResults = ({ countResponse }) => {
+  const wrapperStyle = {
+    padding: '40px',
+    minHeight: '150px',
+    maxHeight: '475px',
+  };
 
-const BeaconQueryResults = ({countResponse}) => {
-
-    const resultsStyle = {
-        height: "300px"
-    };
-
-    const Title = () => {
-      return <h3>Count</h3>
-    }
-
-    return (
-    <div style={resultsStyle}>                      
-      <Card style={{ borderRadius: '10px' }}>
+  return (
+    <div style={wrapperStyle}>
+      <Card
+        style={{ borderRadius: '10px', padding: '10px 33px', width: '1200px', minHeight: '28rem' }}
+        // loading={isFetchingData}
+      >
         <Statistic
-          title={<Title />}
+          title={'Individuals'}
           value={countResponse}
-        //   valueStyle={{ color: '#1890ff' }}
+          valueStyle={{ color: '#75787a' }}
           prefix={<TeamOutlined />}
         />
       </Card>
     </div>
-    );
+  );
 };
 
 export default BeaconQueryResults;
-
