400371f73071bc6ddcba6abd62306d2aa42711b5
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-04-04 17:40:32 -0400

    added comment for typed hooks

--- src/js/hooks.ts
+++ src/js/hooks.ts
@@ -2,5 +2,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// refer to https://react-redux.js.org/using-react-redux/usage-with-typescript#define-typed-hooks for more info
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
