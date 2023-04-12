c96a578f2e5493f2aabba5bc2ccc6339d2deb404
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-04-12 16:17:26 -0400

    Added linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,30 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'react/prop-types': 'off', // disable prop-types since we're using TypeScript
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // allow implicit return types
+    '@typescript-eslint/no-empty-interface': 'off', // allow empty interfaces
+    'no-unused-vars': 'off', // disable no-unused-vars since @typescript-eslint/no-unused-vars does the same
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // enable @typescript-eslint/no-unused-vars
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
   settings: {
     react: {
       version: 'detect',
     },
-  },
-  env: {
-    node: true,
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/prop-types': 0,
   },
 };
