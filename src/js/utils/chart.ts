7d2e72be6ac8947eb582ca5d2af5ef01c5749dbe
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-05-01 13:43:27 -0400

    added type to serialize chart Data

--- src/js/utils/chart.ts
+++ src/js/utils/chart.ts
@@ -1,5 +1,6 @@
 import { Datum } from '@/types/overviewResponse';
+import { ChartData } from '@/types/data';
 
-export const serializeChartData = (chartData: Datum[]) => {
+export const serializeChartData = (chartData: Datum[]): ChartData[] => {
   return chartData.map(({ label, value }) => ({ x: label, y: value }));
 };
