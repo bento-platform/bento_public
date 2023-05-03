e5147bfe76ffa7ba177f8fd4b48eaae2ac24035a
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-05-03 10:44:40 -0400

    WIP temporary commit

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import TabbedDashboard from './components/TabbedDashboard';
 import SiteHeader from './components/SiteHeader';
 import { store } from './store';
@@ -13,27 +14,46 @@ import './i18n';
 import SiteFooter from './components/SiteFooter';
 import { ChartConfigProvider } from 'bento-charts';
 
+const App = () => {
+  const { lang } = useParams<{ lang?: string }>();
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    // if (lang) {
+    //   i18n.changeLanguage(lang);
+    // }
+    console.log('lang', lang);
+  }, [lang, i18n]);
+
+  return (
+    <Layout>
+      <SiteHeader />
+      <Content style={{ padding: '0 30px', marginTop: '10px' }}>
+        <TabbedDashboard />
+      </Content>
+      <SiteFooter />
+    </Layout>
+  );
+};
+
 const BentoApp = () => {
   const { i18n } = useTranslation();
   console.log('i18n.language', i18n.language);
   return (
     <ChartConfigProvider Lng={i18n.language ?? 'en'}>
-      <Layout>
-        <SiteHeader />
-        <Content style={{ padding: '0 30px', marginTop: '10px' }}>
-          <TabbedDashboard />
-        </Content>
-        <SiteFooter />
-      </Layout>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:lang" element={<App />} />
+      </Routes>
     </ChartConfigProvider>
   );
 };
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <Provider store={store}>
-    <BentoApp />
-  </Provider>,
-  rootElement
+    <BrowserRouter>
+      <BentoApp />
+    </BrowserRouter>
+  </Provider>
 );
