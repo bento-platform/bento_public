abf792a9aa088dca07175d546881e153bf9ca7df
Gordon Krieger <gordon.krieger@gmail.com>
2023-06-14 11:56:44 -0400

    serialize in reducers instead of component

--- src/js/features/beacon/beaconQuery.ts
+++ src/js/features/beacon/beaconQuery.ts
@@ -3,18 +3,38 @@ import axios from 'axios';
 import { printAPIError } from '../../utils/error.util';
 import { RootState } from '@/store';
 import { BeaconQueryResponse } from '@/types/beacon';
+import { serializeChartData } from '@/utils/chart';
+import { ChartData } from '@/types/data';
 
-export const makeBeaconQuery = createAsyncThunk<BeaconQueryResponse, void, { state: RootState; rejectValue: string }>('beaconQuery/makeBeaconQuery', async (payload, { getState }) => {
-  const beaconIndividualsEndpoint = getState()?.config?.beaconUrl + '/individuals';
-  return axios
-    .post(beaconIndividualsEndpoint, payload)
-    .then((res) => res.data)
-    .catch(printAPIError);
-});
+export const makeBeaconQuery = createAsyncThunk<BeaconQueryResponse, void, { state: RootState; rejectValue: string }>(
+  'beaconQuery/makeBeaconQuery',
+  async (payload, { getState }) => {
+    const beaconIndividualsEndpoint = getState()?.config?.beaconUrl + '/individuals';
+    return axios
+      .post(beaconIndividualsEndpoint, payload)
+      .then((res) => res.data)
+      .catch(printAPIError);
+  }
+);
 
-const initialState = {
+type BeaconQueryInitialStateType = {
+  isFetchingQueryResponse: boolean;
+  response: BeaconQueryResponse;
+  individualCount: number;
+  biosampleCount: number;
+  biosampleChartData: ChartData[];
+  experimentCount: number;
+  experimentChartData: ChartData[];
+};
+
+const initialState: BeaconQueryInitialStateType = {
   isFetchingQueryResponse: false,
   response: {},
+  individualCount: 0,
+  biosampleCount: 0,
+  biosampleChartData: [],
+  experimentCount: 0,
+  experimentChartData: [],
 };
 
 const beaconQuery = createSlice({
@@ -26,7 +46,15 @@ const beaconQuery = createSlice({
       state.isFetchingQueryResponse = true;
     });
     builder.addCase(makeBeaconQuery.fulfilled, (state, { payload }) => {
-      state.response = payload;
+      if (payload.info) {
+        state.biosampleCount = payload.info.bento?.biosamples?.count;
+        state.biosampleChartData = serializeChartData(payload.info.bento?.biosamples?.sampled_tissue);
+        state.experimentCount = payload.info.bento?.experiments?.count;
+        state.experimentChartData = serializeChartData(payload.info.bento?.experiments?.experiment_type);
+      }
+      if (payload.responseSummary) {
+        state.individualCount = payload.responseSummary.count;
+      }
       state.isFetchingQueryResponse = false;
     });
     builder.addCase(makeBeaconQuery.rejected, (state) => {
