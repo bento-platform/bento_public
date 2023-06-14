61505b586c9687dd9213023a288668ddeb4b5367
Julian <ornatus30@gmail.com>
2023-06-14 16:40:56 -0400

    component for dats download

---
+++ src/js/components/Provenance/DownloadDats.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Space } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
+import { ProvenanceStoreDataset } from '@/types/provenance';
+
+const DATS_FILENAME = 'DATS.json';
+
+const DownloadDats = ({ metadata }: { metadata: ProvenanceStoreDataset }) => {
+    const { t } = useTranslation();
+
+    const downloadData = () => {
+        const blob = new Blob([JSON.stringify(metadata, null, 2)], { type: "application/json" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = DATS_FILENAME;
+        link.click();
+
+        URL.revokeObjectURL(url);
+    };
+
+    const isDatsFileEmpty = Object.keys(metadata.dats_file).length === 0;
+
+    return (
+        <Space style={{ marginTop: '20px', justifyContent: 'center' }}>
+            <Button
+                type="primary"
+                icon={<DownloadOutlined />}
+                onClick={downloadData}
+                disabled={isDatsFileEmpty}
+            >
+                {t("Download DATS File")}
+            </Button>
+        </Space>
+    );
+};
+
+DownloadDats.propTypes = {
+    metadata: PropTypes.object.isRequired,
+};
+
+export default DownloadDats;
