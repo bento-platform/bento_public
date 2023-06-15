8842eb23b311a05d513f9c5c5a0be5a91ba7e8f1
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-06-15 18:00:43 -0400

    translated labels instead of names

--- src/js/components/Beacon/VariantsForm.js
+++ src/js/components/Beacon/VariantsForm.js
@@ -1,9 +1,7 @@
-import React, { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import React from 'react';
 import { Col, Row } from 'antd';
 import VariantInput from './VariantInput';
 import AssemblyIdSelect from './AssemblyIdSelect';
-import { DEFAULT_TRANSLATION } from '@/constants/configConstants';
 
 // TODOs:
 // show which fields are required without removing the ability to leave the form blank
@@ -27,51 +25,46 @@ const FORM_STYLE = {
 };
 const FORM_ROW_GUTTER = [12, 0];
 
+const FORM_FIELDS = {
+  referenceName: {
+    name: 'Chromosome',
+    rules: [{ pattern: CHROMOSOME_REGEX, message: 'Enter a chromosome name, eg: "17" or "X"' }],
+    placeholder: '1-22, X, Y, M',
+    initialValue: '',
+    type: 'string',
+  },
+  start: {
+    name: 'Variant start',
+    rules: [{ pattern: DIGITS_REGEX, message: 'enter a postion number, eg "100"' }],
+    placeholder: 'eg 100',
+    initialValue: '',
+    type: 'number',
+  },
+  end: {
+    name: 'Variant end',
+    rules: [{ pattern: DIGITS_REGEX, message: 'enter a postion number, eg "200"' }],
+    placeholder: 'eg 200',
+    initialValue: '',
+    type: 'number',
+  },
+  referenceBases: {
+    name: 'Reference base(s)',
+    rules: [{ pattern: NUCLEOTIDES_REGEX, message: 'enter any combination of A, C, G, T, or N' }],
+    placeholder: 'A, C, G, G, T or N',
+    initialValue: '',
+    type: 'string',
+  },
+  alternateBases: {
+    name: 'Alternate base(s)',
+    rules: [{ pattern: NUCLEOTIDES_REGEX, message: 'enter any combination of A, C, G, T, or N' }],
+    placeholder: 'A, C, G, T or N',
+    initialValue: '',
+    type: 'string',
+  },
+  assemblyId: { name: 'Assembly ID', rules: [{}], placeholder: '', initialValue: '' },
+};
+
 const VariantsForm = ({ assemblyIdOptions }) => {
-  const { t: td } = useTranslation(DEFAULT_TRANSLATION);
-
-  const FORM_FIELDS = useMemo(
-    () => ({
-      referenceName: {
-        name: td('Chromosome'),
-        rules: [{ pattern: CHROMOSOME_REGEX, message: td('Enter a chromosome name, eg: "17" or "X"') }],
-        placeholder: '1-22, X, Y, M',
-        initialValue: '',
-        type: 'string',
-      },
-      start: {
-        name: td('Variant start'),
-        rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "100"') }],
-        placeholder: 'eg 100',
-        initialValue: '',
-        type: 'number',
-      },
-      end: {
-        name: td('Variant end'),
-        rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "200"') }],
-        placeholder: 'eg 200',
-        initialValue: '',
-        type: 'number',
-      },
-      referenceBases: {
-        name: td('Reference base(s)'),
-        rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
-        placeholder: 'A, C, G, G, T or N',
-        initialValue: '',
-        type: 'string',
-      },
-      alternateBases: {
-        name: td('Alternate base(s)'),
-        rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
-        placeholder: 'A, C, G, T or N',
-        initialValue: '',
-        type: 'string',
-      },
-      assemblyId: { name: 'Assembly ID', rules: [{}], placeholder: '', initialValue: '' },
-    }),
-    [td]
-  );
-
   return (
     <div style={FORM_STYLE}>
       <Row gutter={FORM_ROW_GUTTER}>
