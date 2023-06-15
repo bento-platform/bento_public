96e6546954a97e818160cc2a3781164ecdedd75c
Julian <ornatus30@gmail.com>
2023-06-15 10:57:07 -0400

    lint

--- src/js/components/Provenance/DownloadDats.tsx
+++ src/js/components/Provenance/DownloadDats.tsx
@@ -8,37 +8,32 @@ import { ProvenanceStoreDataset } from '@/types/provenance';
 const DATS_FILENAME = 'DATS.json';
 
 const DownloadDats = ({ metadata }: { metadata: ProvenanceStoreDataset }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const downloadData = () => {
-        const blob = new Blob([JSON.stringify(metadata, null, 2)], { type: "application/json" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = DATS_FILENAME;
-        link.click();
+  const downloadData = () => {
+    const blob = new Blob([JSON.stringify(metadata, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = DATS_FILENAME;
+    link.click();
 
-        URL.revokeObjectURL(url);
-    };
+    URL.revokeObjectURL(url);
+  };
 
-    const isDatsFileEmpty = metadata && metadata.dats_file ? Object.keys(metadata.dats_file).length === 0 : true;
+  const isDatsFileEmpty = metadata && metadata.dats_file ? Object.keys(metadata.dats_file).length === 0 : true;
 
-    return (
-        <Space style={{ marginTop: '20px', justifyContent: 'center' }}>
-            <Button
-                type="primary"
-                icon={<DownloadOutlined />}
-                onClick={downloadData}
-                disabled={isDatsFileEmpty}
-            >
-                {t("Download DATS File")}
-            </Button>
-        </Space>
-    );
+  return (
+    <Space style={{ marginTop: '20px', justifyContent: 'center' }}>
+      <Button type="primary" icon={<DownloadOutlined />} onClick={downloadData} disabled={isDatsFileEmpty}>
+        {t('Download DATS File')}
+      </Button>
+    </Space>
+  );
 };
 
 DownloadDats.propTypes = {
-    metadata: PropTypes.object.isRequired,
+  metadata: PropTypes.object.isRequired,
 };
 
 export default DownloadDats;
