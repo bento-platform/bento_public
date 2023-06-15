c8cd924728c0856c40f86ca9c89e4fbb77a71a46
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-06-15 13:48:30 -0400

    optimized variants form

--- src/js/components/Beacon/VariantsForm.js
+++ src/js/components/Beacon/VariantsForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Col, Row } from 'antd';
 import VariantInput from './VariantInput';
@@ -30,44 +30,47 @@ const FORM_ROW_GUTTER = [12, 0];
 const VariantsForm = ({ assemblyIdOptions }) => {
   const { t: td } = useTranslation(DEFAULT_TRANSLATION);
 
-  const FORM_FIELDS = {
-    referenceName: {
-      name: td('Chromosome'),
-      rules: [{ pattern: CHROMOSOME_REGEX, message: td('Enter a chromosome name, eg: "17" or "X"') }],
-      placeholder: '1-22, X, Y, M',
-      initialValue: '',
-      type: 'string',
-    },
-    start: {
-      name: td('Variant start'),
-      rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "100"') }],
-      placeholder: 'eg 100',
-      initialValue: '',
-      type: 'number',
-    },
-    end: {
-      name: td('Variant end'),
-      rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "200"') }],
-      placeholder: 'eg 200',
-      initialValue: '',
-      type: 'number',
-    },
-    referenceBases: {
-      name: td('Reference base(s)'),
-      rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
-      placeholder: 'A, C, G, G, T or N',
-      initialValue: '',
-      type: 'string',
-    },
-    alternateBases: {
-      name: td('Alternate base(s)'),
-      rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
-      placeholder: 'A, C, G, T or N',
-      initialValue: '',
-      type: 'string',
-    },
-    assemblyId: { name: 'Assembly ID', rules: [{}], placeholder: '', initialValue: '' },
-  };
+  const FORM_FIELDS = useMemo(
+    () => ({
+      referenceName: {
+        name: td('Chromosome'),
+        rules: [{ pattern: CHROMOSOME_REGEX, message: td('Enter a chromosome name, eg: "17" or "X"') }],
+        placeholder: '1-22, X, Y, M',
+        initialValue: '',
+        type: 'string',
+      },
+      start: {
+        name: td('Variant start'),
+        rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "100"') }],
+        placeholder: 'eg 100',
+        initialValue: '',
+        type: 'number',
+      },
+      end: {
+        name: td('Variant end'),
+        rules: [{ pattern: DIGITS_REGEX, message: td('enter a postion number, eg "200"') }],
+        placeholder: 'eg 200',
+        initialValue: '',
+        type: 'number',
+      },
+      referenceBases: {
+        name: td('Reference base(s)'),
+        rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
+        placeholder: 'A, C, G, G, T or N',
+        initialValue: '',
+        type: 'string',
+      },
+      alternateBases: {
+        name: td('Alternate base(s)'),
+        rules: [{ pattern: NUCLEOTIDES_REGEX, message: td('enter any combination of A, C, G, T, or N') }],
+        placeholder: 'A, C, G, T or N',
+        initialValue: '',
+        type: 'string',
+      },
+      assemblyId: { name: 'Assembly ID', rules: [{}], placeholder: '', initialValue: '' },
+    }),
+    [td]
+  );
 
   return (
     <div style={FORM_STYLE}>
