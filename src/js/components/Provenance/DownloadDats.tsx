45b8f315560883a43b39445f66aec60918e04f33
Julian <ornatus30@gmail.com>
2023-08-02 09:13:17 -0400

    Refactor component to fetch data from api

--- src/js/components/Provenance/DownloadDats.tsx
+++ src/js/components/Provenance/DownloadDats.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
-import { Button, Space } from 'antd';
+import { Button, Space, message } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import { ProvenanceStoreDataset } from '@/types/provenance';
@@ -9,23 +10,38 @@ const DATS_FILENAME = 'DATS.json';
 
 const DownloadDats = ({ metadata }: { metadata: ProvenanceStoreDataset }) => {
   const { t } = useTranslation();
+  const [isLoading, setLoading] = useState(false);
 
-  const downloadData = () => {
-    const blob = new Blob([JSON.stringify(metadata, null, 2)], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = DATS_FILENAME;
-    link.click();
-
-    URL.revokeObjectURL(url);
+  const downloadData = async () => {
+    setLoading(true);
+    try {
+      const id = metadata.identifier;
+      const response = await axios.get(`/datasets/${id}/dats`);
+      const blob = new Blob([JSON.stringify(response.data, null, 2)], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = DATS_FILENAME;
+      link.click();
+      URL.revokeObjectURL(url);
+    } catch (err) {
+      message.error('Failed to download DATS file');
+    } finally {
+      setLoading(false);
+    }
   };
 
   const isDatsFileEmpty = metadata && metadata.dats_file ? Object.keys(metadata.dats_file).length === 0 : true;
 
   return (
     <Space style={{ marginTop: '20px', justifyContent: 'center' }}>
-      <Button type="primary" icon={<DownloadOutlined />} onClick={downloadData} disabled={isDatsFileEmpty}>
+      <Button
+        type="primary"
+        icon={<DownloadOutlined />}
+        onClick={downloadData}
+        disabled={isDatsFileEmpty}
+        loading={isLoading}
+      >
         {t('Download DATS File')}
       </Button>
     </Space>
