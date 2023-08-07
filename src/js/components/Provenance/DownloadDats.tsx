4284b8e43cae9fa46b92982cf1f0c6c3817e90fa
Julian <ornatus30@gmail.com>
2023-08-07 15:59:27 -0400

    Refactor DownloadDats for use window.location.href for download dats file

--- src/js/components/Provenance/DownloadDats.tsx
+++ src/js/components/Provenance/DownloadDats.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Space, message } from 'antd';
+import { Button, Space } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import { ProvenanceStoreDataset } from '@/types/provenance';
 
-const DATS_FILENAME = 'DATS.json';
-
 const DownloadDats = ({ metadata }: { metadata: ProvenanceStoreDataset }) => {
   const { t } = useTranslation();
-  const [isLoading, setLoading] = useState(false);
 
-  const downloadData = async () => {
-    setLoading(true);
-    try {
-      const id = metadata.identifier;
-      const response = await axios.get(`/datasets/${id}/dats`);
-      const blob = new Blob([JSON.stringify(response.data, null, 2)], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = DATS_FILENAME;
-      link.click();
-      URL.revokeObjectURL(url);
-    } catch (err) {
-      message.error('Failed to download DATS file');
-    } finally {
-      setLoading(false);
-    }
+  const exportData = () => {
+    const id = metadata.identifier;
+    window.location.href = `/datasets/${id}/dats`;
   };
 
   const isDatsFileEmpty = metadata && metadata.dats_file ? Object.keys(metadata.dats_file).length === 0 : true;
@@ -38,9 +20,8 @@ const DownloadDats = ({ metadata }: { metadata: ProvenanceStoreDataset }) => {
       <Button
         type="primary"
         icon={<DownloadOutlined />}
-        onClick={downloadData}
+        onClick={exportData}
         disabled={isDatsFileEmpty}
-        loading={isLoading}
       >
         {t('Download DATS File')}
       </Button>
