0bfb64b42fec7cea6fc269186c8cb74b4c0aec11
David Lougheed <david.lougheed@gmail.com>
2023-09-07 11:33:37 -0400

    fix: properly handle api errors in about requests

--- src/js/features/content/content.store.ts
+++ src/js/features/content/content.store.ts
@@ -3,19 +3,21 @@ import axios from 'axios';
 import { partialAboutUrl } from '@/constants/contentConstants';
 import { printAPIError } from '@/utils/error.util';
 
-export const makeGetAboutRequest = createAsyncThunk('content/getAboutHTML', async () => {
-  const en = await axios
-    .get(`${partialAboutUrl}/en_about.html`)
-    .then((res) => res.data)
-    .catch(printAPIError);
+export const makeGetAboutRequest = createAsyncThunk(
+  'content/getAboutHTML',
+  async (_, { rejectWithValue }) => {
+    const en = await axios
+      .get(`${partialAboutUrl}/en_about.html`)
+      .then((res) => res.data)
+      .catch(printAPIError(rejectWithValue));
 
-  const fr = await axios
-    .get(`${partialAboutUrl}/fr_about.html`)
-    .then((res) => res.data)
-    .catch(printAPIError);
+    const fr = await axios
+      .get(`${partialAboutUrl}/fr_about.html`)
+      .then((res) => res.data)
+      .catch(printAPIError(rejectWithValue));
 
-  return { en, fr };
-});
+    return { en, fr };
+  });
 
 export type ContentState = {
   isFetchingAbout: boolean;
