218729f11e42da6244b7b1a3909645d3a4bbb341
David Lougheed <david.lougheed@gmail.com>
2023-09-07 11:36:01 -0400

    lint

--- src/js/features/content/content.store.ts
+++ src/js/features/content/content.store.ts
@@ -3,21 +3,19 @@ import axios from 'axios';
 import { partialAboutUrl } from '@/constants/contentConstants';
 import { printAPIError } from '@/utils/error.util';
 
-export const makeGetAboutRequest = createAsyncThunk(
-  'content/getAboutHTML',
-  async (_, { rejectWithValue }) => {
-    const en = await axios
-      .get(`${partialAboutUrl}/en_about.html`)
-      .then((res) => res.data)
-      .catch(printAPIError(rejectWithValue));
+export const makeGetAboutRequest = createAsyncThunk('content/getAboutHTML', async (_, { rejectWithValue }) => {
+  const en = await axios
+    .get(`${partialAboutUrl}/en_about.html`)
+    .then((res) => res.data)
+    .catch(printAPIError(rejectWithValue));
 
-    const fr = await axios
-      .get(`${partialAboutUrl}/fr_about.html`)
-      .then((res) => res.data)
-      .catch(printAPIError(rejectWithValue));
+  const fr = await axios
+    .get(`${partialAboutUrl}/fr_about.html`)
+    .then((res) => res.data)
+    .catch(printAPIError(rejectWithValue));
 
-    return { en, fr };
-  });
+  return { en, fr };
+});
 
 export type ContentState = {
   isFetchingAbout: boolean;
