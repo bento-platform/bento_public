2b753e9ba9d8744431e8e4c3f7c04a097fdb8bcf
David Lougheed <david.lougheed@gmail.com>
2023-09-15 10:20:23 -0400

    lint

--- src/js/components/Provenance/Tables/BaseProvenanceTable.tsx
+++ src/js/components/Provenance/Tables/BaseProvenanceTable.tsx
@@ -3,12 +3,7 @@ import { Table, TableProps } from 'antd';
 import { AnyObject } from 'antd/es/table/Table';
 
 const BaseProvenanceTable = <T extends AnyObject>(props: TableProps<T>) => (
-  <Table
-    bordered={true}
-    pagination={false}
-    size="small"
-    {...props}
-  />
+  <Table bordered={true} pagination={false} size="small" {...props} />
 );
 
 export default BaseProvenanceTable;
