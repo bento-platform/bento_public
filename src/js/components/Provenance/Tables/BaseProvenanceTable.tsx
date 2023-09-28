608918275bdd751025b7eb3e51f9feb95f1500a9
David Lougheed <david.lougheed@gmail.com>
2023-09-28 14:40:16 -0400

    chore: update all outdated dependencies

--- src/js/components/Provenance/Tables/BaseProvenanceTable.tsx
+++ src/js/components/Provenance/Tables/BaseProvenanceTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Table, TableProps } from 'antd';
-import { AnyObject } from 'antd/es/table/Table';
+import { AnyObject } from 'antd/es/_util/type';
 
 const BaseProvenanceTable = <T extends AnyObject>(props: TableProps<T>) => (
   <Table bordered={true} pagination={false} size="small" {...props} />
