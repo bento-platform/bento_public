2c0cff86bf1db30151e96197913f5e054144e67a
Julian <ornatus30@gmail.com>
2023-10-11 14:28:22 -0400

    types for last ingestion by datatypes

---
+++ src/js/types/lastIngestionDataTypeResponse.ts
@@ -0,0 +1,10 @@
+export interface LastIngestionDataTypeResponse {
+    count: number | null;
+    id: string;
+    label: string;
+    last_ingested: string | null;
+    queryable: boolean;
+  }
+  
+  export type DataResponseArray = LastIngestionDataTypeResponse[];
+  
