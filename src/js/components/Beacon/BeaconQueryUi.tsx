d6fe2e48650499b6b22905c3c25d53235828ebe0
Gordon Krieger <gordon.krieger@gmail.com>
2023-11-08 12:22:41 -0500

    beacon form verification / user feedback

--- src/js/components/Beacon/BeaconQueryUi.tsx
+++ src/js/components/Beacon/BeaconQueryUi.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ReactNode } from 'react';
 import { useAppSelector, useAppDispatch } from '@/hooks';
 import { useTranslation } from 'react-i18next';
 import { Button, Card, Col, Form, Row, Tooltip } from 'antd';
@@ -6,6 +6,7 @@ import { InfoCircleOutlined } from '@ant-design/icons';
 import Filters from './Filters';
 import BeaconSearchResults from './BeaconSearchResults';
 import VariantsForm from './VariantsForm';
+import BeaconErrorMessage from './BeaconErrorMessage';
 import { makeBeaconQuery } from '@/features/beacon/beaconQuery.store';
 import { BeaconQueryPayload, FormFilter, FormValues, PayloadFilter, PayloadVariantsQuery } from '@/types/beacon';
 import {
@@ -20,13 +21,9 @@ import {
 import { DEFAULT_TRANSLATION } from '@/constants/configConstants';
 
 // TODOs
-// form verification
 // example searches, either hardcoded or configurable
-// better help text (#1691)
+// more intuitive variants ui
 
-const VARIANTS_HELP =
-  'Variants search requires the fields Chromosome, Variant start, Assembly ID, and at least one of Variant end or Alternate base(s).';
-const METADATA_HELP = 'Search over clinical or phenotypic properties.';
 const STARTER_FILTER = { index: 1, active: true };
 
 const BeaconQueryUi = () => {
@@ -34,6 +31,12 @@ const BeaconQueryUi = () => {
   const [form] = Form.useForm();
   const [filters, setFilters] = useState<FormFilter[]>([STARTER_FILTER]);
   const [hasVariants, setHasVariants] = useState<boolean>(false);
+  const [hasFormError, setHasFormError] = useState<boolean>(false);
+  const [formErrorMessage, setFormErrorMessage] = useState<string>('');
+
+  // remember if user closed alert, so we can force re-render of a new one later
+  const [errorAlertClosed, setErrorAlertClosed] = useState<boolean>(false);
+
   const isFetchingBeaconConfig = useAppSelector((state) => state.beaconConfig.isFetchingBeaconConfig);
   const beaconAssemblyIds = useAppSelector((state) => state.beaconConfig.beaconAssemblyIds);
   const querySections = useAppSelector((state) => state.query.querySections);
@@ -43,6 +46,29 @@ const BeaconQueryUi = () => {
   const launchEmptyQuery = () => dispatch(makeBeaconQuery(requestPayload({}, [])));
   const formInitialValues = { 'Assembly ID': beaconAssemblyIds.length === 1 && beaconAssemblyIds[0] };
   const uiInstructions = hasVariants ? 'Search by genomic variants, clinical metadata or both.' : '';
+
+  // complexity of instructions suggests the form isn't intuitive enough
+  const variantsInstructions = (
+    <>
+      <h4>{td('Variant search')}</h4>
+      <p>
+        {td(
+          'To search for all variants inside a range: fill both "Variant start" and "Variant end", all variants inside the range will be returned. You can optionally filter by reference or alternate bases.'
+        )}
+      </p>
+      <p>
+        {td(
+          'To search for a variant at a particular position, either set "Variant end" to the same value in "Variant start", or fill in values for both reference and alternate bases.'
+        )}
+      </p>
+      <p>{td('"Chromosome", "Variant start" and "Assembly ID" are always required.')}</p>
+      <p>
+        {td('Coordinates are one-based.')} {td('Leave this form blank to search by metadata only.')}
+      </p>
+    </>
+  );
+
+  const metadataInstructions = <p>{td('Search over clinical or phenotypic properties.')}</p>;
 
   useEffect(() => {
     // wait for config
@@ -113,7 +139,44 @@ const BeaconQueryUi = () => {
 
   // form utils
 
+  const variantsFormValid = (formValues: FormValues) => {
+    // valid variant form states:
+    // empty except possibly autofilled assemblyID (no variant query)
+    // chrom, start, assemblyID, end (range query)
+    // chrom, start, assemblyID, alt, ref (sequence query)
+    // https://docs.genomebeacons.org/variant-queries/
+
+    // as an alternative, we could require "end" always, then form logic would be less convoluted
+    // just set start=end to search for SNPs
+
+    const empty = !(
+      formValues['Chromosome'] ||
+      formValues['Variant start'] ||
+      formValues['Variant end'] ||
+      formValues['Reference base(s)'] ||
+      formValues['Alternate base(s)']
+    );
+    const rangeQuery =
+      formValues['Chromosome'] && formValues['Variant start'] && formValues['Variant end'] && formValues['Assembly ID'];
+    const sequenceQuery =
+      formValues['Chromosome'] &&
+      formValues['Variant start'] &&
+      formValues['Assembly ID'] &&
+      formValues['Reference base(s)'] &&
+      formValues['Alternate base(s)'];
+    return empty || rangeQuery || sequenceQuery;
+  };
+
   const handleFinish = (formValues: FormValues) => {
+    // if bad form, block submit and show user error
+    if (!variantsFormValid(formValues)) {
+      setHasFormError(true);
+      setErrorAlertClosed(false);
+      setFormErrorMessage('variants form should include either an end position or both reference and alternate bases');
+      return;
+    }
+
+    setHasFormError(false);
     const jsonPayload = packageBeaconJSON(formValues);
     dispatch(makeBeaconQuery(jsonPayload));
   };
@@ -122,12 +185,24 @@ const BeaconQueryUi = () => {
     setFilters([STARTER_FILTER]);
     form.resetFields();
     form.setFieldsValue(formInitialValues);
+    setHasFormError(false);
     launchEmptyQuery();
   };
 
-  const SearchToolTip = ({ text }: { text: string }) => {
+  const handleValuesChange = (_: Partial<FormValues>, allValues: FormValues) => {
+    form.validateFields(['Chromosome', 'Variant start', 'Variant end', 'Reference base(s)', 'Alternate base(s)']);
+
+    // clear any existing errors if form now valid
+    if (variantsFormValid(allValues)) {
+      setHasFormError(false);
+    }
+
+    // can also check filter values here
+  };
+
+  const SearchToolTip = ({ content }: { content: ReactNode }) => {
     return (
-      <Tooltip title={td(text)}>
+      <Tooltip title={content}>
         <InfoCircleOutlined />
       </Tooltip>
     );
@@ -143,7 +218,7 @@ const BeaconQueryUi = () => {
         headStyle={CARD_HEAD_STYLE}
       >
         <p style={{ margin: '-8px 0 8px 0', padding: '0', color: 'grey' }}>{td(uiInstructions)}</p>
-        <Form form={form} onFinish={handleFinish} layout="vertical">
+        <Form form={form} onFinish={handleFinish} layout="vertical" onValuesChange={handleValuesChange}>
           <Row gutter={FORM_ROW_GUTTERS}>
             {hasVariants && (
               <Col xs={24} lg={12}>
@@ -152,7 +227,7 @@ const BeaconQueryUi = () => {
                   style={CARD_STYLE}
                   headStyle={CARD_HEAD_STYLE}
                   bodyStyle={CARD_BODY_STYLE}
-                  extra={<SearchToolTip text={VARIANTS_HELP} />}
+                  extra={<SearchToolTip content={variantsInstructions} />}
                 >
                   <VariantsForm beaconAssemblyIds={beaconAssemblyIds} />
                 </Card>
@@ -164,7 +239,7 @@ const BeaconQueryUi = () => {
                 style={CARD_STYLE}
                 headStyle={CARD_HEAD_STYLE}
                 bodyStyle={CARD_BODY_STYLE}
-                extra={<SearchToolTip text={METADATA_HELP} />}
+                extra={<SearchToolTip content={metadataInstructions} />}
               >
                 <Filters filters={filters} setFilters={setFilters} form={form} querySections={querySections} />
               </Card>
@@ -172,6 +247,12 @@ const BeaconQueryUi = () => {
           </Row>
           <Row>
             <Col span={24}>
+              {hasFormError && !errorAlertClosed && (
+                <BeaconErrorMessage
+                  message={`Beacon error: ${formErrorMessage}`}
+                  setErrorAlertClosed={setErrorAlertClosed}
+                />
+              )}
               <div style={BUTTON_AREA_STYLE}>
                 <Button type="primary" htmlType="submit" style={BUTTON_STYLE}>
                   {td('Search Beacon')}
