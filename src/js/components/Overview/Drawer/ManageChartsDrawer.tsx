ed0d92aa9874d85b8b244dcf483229d34cf729c7
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-11-29 15:24:18 -0500

    Added show all and hide all functionality

--- src/js/components/Overview/Drawer/ManageChartsDrawer.tsx
+++ src/js/components/Overview/Drawer/ManageChartsDrawer.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
 
-import { Drawer, DrawerProps, Typography } from 'antd';
+import { Button, Drawer, DrawerProps, Flex, Space, Typography } from 'antd';
 const { Title } = Typography;
 
 import ChartTree from './ChartTree';
 
 import { ChartDataField } from '@/types/data';
-import { useAppSelector, useTranslationCustom, useTranslationDefault } from '@/hooks';
+import { useAppSelector, useAppDispatch, useTranslationCustom, useTranslationDefault } from '@/hooks';
+import { hideAllSectionCharts, setAllDisplayedCharts } from '@/features/data/data.store';
 
 const ManageChartsDrawer = ({ onManageDrawerClose, manageDrawerVisible }: ManageChartsDrawerProps) => {
   const t = useTranslationCustom();
   const td = useTranslationDefault();
 
+  const dispatch = useAppDispatch();
+
   const sections = useAppSelector((state) => state.data.sections);
 
   return (
-    <Drawer title={td('Manage Charts')} placement="right" onClose={onManageDrawerClose} open={manageDrawerVisible}>
+    <Drawer
+      title={td('Manage Charts')}
+      placement="right"
+      onClose={onManageDrawerClose}
+      open={manageDrawerVisible}
+      extra={
+        <Button
+          size="small"
+          onClick={() => {
+            dispatch(setAllDisplayedCharts({}));
+          }}
+        >
+          Show All
+        </Button>
+      }
+    >
       {sections.map(({ sectionTitle, charts }: { sectionTitle: string; charts: ChartDataField[] }, i: number) => (
         <div key={i}>
-          <Title level={5}>{t(sectionTitle)}</Title>
+          <Flex justify="space-between" align="center" style={{ padding: "10px 0" }}>
+            <Title level={5} style={{ margin: '0' }}>
+              {t(sectionTitle)}
+            </Title>
+            <Space>
+            <Button
+              size="small"
+              onClick={() => {
+                dispatch(setAllDisplayedCharts({ section: sectionTitle }));
+              }}
+            >
+              Show All
+            </Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  dispatch(hideAllSectionCharts({ section: sectionTitle }));
+                }}
+              >
+                Hide All
+              </Button>
+            </Space>
+          </Flex>
           <ChartTree charts={charts} section={sectionTitle} />
         </div>
       ))}
