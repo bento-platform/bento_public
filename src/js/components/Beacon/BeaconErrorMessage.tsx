330a651927bc23b5cbee80950f4e39b96fd27211
Gordon Krieger <gordon.krieger@gmail.com>
2023-12-11 15:02:08 -0500

    rm dead imports

--- src/js/components/Beacon/BeaconErrorMessage.tsx
+++ src/js/components/Beacon/BeaconErrorMessage.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { Alert } from 'antd';
 
 const BeaconErrorMessage = ({ message, onClose }: BeaconErrorMessageProps) => {
