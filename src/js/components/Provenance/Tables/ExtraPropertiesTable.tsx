27b2ba9947daf4ed68bb8384c6ca449c97420202
Gordon Krieger <gordon.krieger@gmail.com>
2024-03-21 12:10:13 -0400

    Fragment instead of span

--- src/js/components/Provenance/Tables/ExtraPropertiesTable.tsx
+++ src/js/components/Provenance/Tables/ExtraPropertiesTable.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { Fragment, useMemo } from 'react';
 
 import { ColumnsType } from 'antd/es/table';
 
@@ -20,10 +20,10 @@ const ExtraPropertiesTable = ({ extraProperties }: ExtraPropertiesTableProps) =>
           dataIndex: 'values',
           render: (_, { values }) =>
             values.map((v, i) => (
-              <span key={i}>
+              <Fragment key={i}>
                 <LinkIfUrl text={v.value.toString()} />
                 {i < values.length - 1 ? '; ' : ''}
-              </span>
+              </Fragment>
             )),
         },
       ] as ColumnsType<ExtraProperty>,
