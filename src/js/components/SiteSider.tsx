9083386ac69ac3e0361b08d6e856f690e0250e4a
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-05-06 14:31:04 -0400

    temp

--- src/js/components/SiteSider.tsx
+++ src/js/components/SiteSider.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import { Col, Layout, Row } from 'antd';
+import { Col, Flex, Layout, Row } from 'antd';
 
 const { Sider } = Layout;
+
+const iconBackgroundStyle = {
+  backgroundColor: '#FFFFFF25',
+  borderRadius: '10px',
+  padding: '10px',
+  margin: '16px',
+};
 
 const SiteSider = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -12,26 +19,13 @@ const SiteSider = () => {
       onCollapse={setCollapsed}
       style={{ display: 'flex', alignItems: 'center', height: '100vh' }}
     >
-      <Row justify="center" style={{ width: '100%' }}>
-        <Col>
-          <div
-            style={{
-              backgroundColor: '#FFFFFF25',
-              borderRadius: '10px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100px', // Fixed height of the inner div
-              width: '80%', // Adjust the width based on design requirements
-              maxWidth: '300px', // Prevents the div from becoming too wide on larger screens
-            }}
-          >
-            <a href="/">
-              <img style={{ height: '32px' }} src="/public/assets/branding.png" alt="logo" />
-            </a>
-          </div>
-        </Col>
-      </Row>
+      <Flex style={{ width: '100%' }} justify="center" align="center">
+        <Flex style={iconBackgroundStyle} justify="center" align="center">
+          <a href="/">
+            <img style={{ height: '32px' }} src="/public/assets/branding.png" alt="logo" />
+          </a>
+        </Flex>
+      </Flex>
     </Sider>
   );
 };
