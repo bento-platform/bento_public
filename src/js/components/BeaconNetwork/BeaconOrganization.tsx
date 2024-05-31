1a7a63bfb0f4f47f3441db18db2510c98a32a9e6
Gordon Krieger <gordon.krieger@gmail.com>
2024-05-31 16:40:39 -0400

    beacon org

---
+++ src/js/components/BeaconNetwork/BeaconOrganization.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, Typography } from 'antd';
+import { BeaconOrganizationType } from '@/types/beacon';
+const { Title, Text, Link } = Typography;
+
+const LINK_STYLE = { padding: '10px' };
+const ORG_CARD_STYLE = { background: 'hotpink' };
+const CARD_STYLES = {
+  body: { background: '#f5f5f5', width: '100%' },
+};
+
+const BeaconOrganization = ({ organization, bentoUrl, description }: BeaconOrganizationProps) => {
+  return (
+    <Card styles={CARD_STYLES}>
+      <Text>{description}</Text>
+      <div style={{ display: 'flex' }}>
+        <Link href={organization.welcomeUrl} target="_blank" style={LINK_STYLE}>
+          Home Page
+        </Link>
+        <Link href={bentoUrl} target="_blank" style={LINK_STYLE}>
+          Bento
+        </Link>
+      </div>
+    </Card>
+  );
+};
+
+export interface BeaconOrganizationProps {
+  organization: BeaconOrganizationType;
+  bentoUrl: string | undefined;
+  description: string | undefined;
+}
+
+export default BeaconOrganization;
