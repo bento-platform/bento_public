e1fffc0e6b6bae7162e1568e93cf1a14d9400fbb
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-06-05 13:55:11 -0400

    moved non-hook out of hooks

--- src/js/hooks.ts
+++ src/js/hooks.ts
@@ -50,16 +50,3 @@ export const useBeaconWithAuthIfAllowed = () => {
     }
   }, [dispatch, hasPermission]);
 };
-
-// ################### ROUTER HOOKS ###################
-
-export const getCurrentPage = (): string => {
-  const location = window.location;
-  const pathArray = location.pathname.split('/');
-  const validPages = ['overview', 'search', 'beacon', 'provenance'];
-  if (pathArray.length > 2 && validPages.includes(pathArray[2])) {
-    return pathArray[2];
-  } else {
-    return 'overview';
-  }
-};
