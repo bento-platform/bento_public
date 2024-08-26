3c6b10f464e703d65548f66f7b76e061f0929aaf
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-08-26 11:06:11 -0400

    resolved all react-hook-linter issues after rebase

--- src/js/components/BentoAppRouter.tsx
+++ src/js/components/BentoAppRouter.tsx
@@ -50,7 +50,7 @@ const ScopedRoute = () => {
       const newPathString = '/' + newPath.join('/');
       navigate(newPathString, { replace: true });
     }
-  }, [projects, projectId, datasetId, dispatch]);
+  }, [projects, projectId, datasetId, dispatch, navigate]);
 
   return <Outlet />;
 };
@@ -70,7 +70,7 @@ const BentoAppRouter = () => {
     dispatch(makeGetProvenanceRequest());
     dispatch(makeGetKatsuPublic());
     dispatch(fetchKatsuData());
-  }, [selectedScope]);
+  }, [dispatch, selectedScope]);
 
   useEffect(() => {
     dispatch(getProjects());
