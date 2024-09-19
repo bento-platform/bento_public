c1a93c32ad09a86321ba5f7ddbfbf5122587e7d3
Gordon Krieger <gordon.krieger@gmail.com>
2024-09-19 15:26:20 -0400

    plain redux thunk for beacon network query

--- src/js/features/beacon/networkQuery.store.ts
+++ src/js/features/beacon/networkQuery.store.ts
@@ -1,6 +1,6 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 // import { makeAuthorizationHeader } from 'bento-auth-js';
-import { RootState } from '@/store';
+import type { RootState, AppDispatch } from '@/store';
 import { beaconApiError } from '@/utils/beaconApiError';
 import { BeaconQueryPayload } from '@/types/beacon';
 import { networkBeaconQuery } from './networkBeaconQuery.store';
@@ -13,29 +13,13 @@ const queryUrl = (beaconId: string, endpoint: string): string => {
   return BEACON_NETWORK_URL + '/beacons/' + beaconId + endpoint;
 };
 
-// probably more biolerplate below than needed
-// we only really need to dispatch networkBeaconQuery() once for each beacon in the network
-// correct implementation is left as an exercise for the reader
-// These options all appear to have the same behaviour below:
-//    return await Promise.all()
-//    return Promise.all()
-//    Promise.all() (no return statement)
-
-
-export const beaconNetworkQuery = createAsyncThunk<void, BeaconQueryPayload, { state: RootState; rejectValue: string }>(
-  'beaconNetwork/beaconNetworkQuery',
-  async (payload, { getState, rejectWithValue, dispatch }) => {
-    // no auth in network prototype
-    // const token = getState().auth.accessToken;
-    // const headers = makeAuthorizationHeader(token);
-
-
+// plain redux thunk, no state updates
+// dispatch a query for each beacon in the network
+export const beaconNetworkQuery =
+  (payload: BeaconQueryPayload) => (dispatch: AppDispatch, getState: () => RootState) => {
     const beacons = getState().beaconNetwork.beacons;
-    return await Promise.all(
-      beacons.map((b) => {
-        const url = queryUrl(b.id, DEFAULT_QUERY_ENDPOINT);
-        return dispatch(networkBeaconQuery({ beaconId: b.id, url: url, payload: payload }));
-      })
-    ).catch(beaconApiError(rejectWithValue));
-  }
-);
+    beacons.forEach((b) => {
+      const url = queryUrl(b.id, DEFAULT_QUERY_ENDPOINT);
+      dispatch(networkBeaconQuery({ beaconId: b.id, url: url, payload: payload }));
+    });
+  };
