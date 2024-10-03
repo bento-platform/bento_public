88bf3614b3d54040dbc9534172008beaa8807f32
Gordon Krieger <gordon.krieger@gmail.com>
2024-10-03 12:14:55 -0400

    disambiguate beacon network node from local beacon

--- src/js/components/Beacon/BeaconNetwork/NetworkBeacons.tsx
+++ src/js/components/Beacon/BeaconNetwork/NetworkBeacons.tsx
@@ -1,16 +1,16 @@
 import { useAppSelector } from '@/hooks';
 import type { NetworkBeacon } from '@/types/beaconNetwork';
-import BeaconDetails from './NetworkNodeDetails/BeaconDetails';
+import NodeDetails from './NetworkNodeDetails/NodeDetails';
 
 const NetworkBeacons = ({ beacons }: NetworkBeaconsProps) => {
   const beaconResponses = useAppSelector((state) => state.beaconNetworkResponse.beacons);
 
-  // for now, render all beacons from the start and update as they respond (BeaconDetails component is memoized)
+  // for now, render all beacons from the start and update as they respond (NodeDetails component is memoized)
   // they could be shown optionally if network is very large (ie could just show only beacons with non-zero responses)
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', maxWidth: '1200px', width: '100%' }}>
       {beacons.map((b) => (
-        <BeaconDetails beacon={b} key={b.id} response={beaconResponses[b.id] ?? {}} />
+        <NodeDetails beacon={b} key={b.id} response={beaconResponses[b.id] ?? {}} />
       ))}
     </div>
   );
