7303a337be6e5dd8e684c75787d00b0efe7e20dd
David Lougheed <david.lougheed@gmail.com>
2024-10-10 13:54:25 -0400

    style: fix responsiveness issues for network

--- src/js/components/Beacon/BeaconNetwork/NetworkBeacons.tsx
+++ src/js/components/Beacon/BeaconNetwork/NetworkBeacons.tsx
@@ -9,9 +9,9 @@ const NetworkBeacons = ({ beacons }: NetworkBeaconsProps) => {
   // for now, render all beacons from the start and update as they respond (NodeDetails component is memoized)
   // they could be shown optionally if network is very large (ie could just show only beacons with non-zero responses)
   return (
-    <Row gutter={[8, 8]} style={{ maxWidth: 1200 }}>
+    <Row gutter={[8, 8]} style={{ width: '100%', maxWidth: 1200 }}>
       {beacons.map((b) => (
-        <Col lg={24} xl={12} key={b.id}>
+        <Col span={24} xl={12} key={b.id}>
           <NodeDetails beacon={b} key={b.id} response={beaconResponses[b.id] ?? {}} />
         </Col>
       ))}
