fbb12c61c95f1bf50cbe08fb263c68b218dff33e
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2024-11-14 11:53:38 -0500

    typed demo data

--- src/js/components/Provenance/Catalogue.tsx
+++ src/js/components/Provenance/Catalogue.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Card, Space } from 'antd';
 import demoData from '../../../public/data/dataset_catalogue_demo.json';
+import CatalogueCard from '@/components/Provenance/CatalogueCard';
 
-const { projects } = demoData;
+type Dataset = {
+  name: string;
+  description: string;
+  samples: string;
+  dataType: string;
+};
+
+export type Project = {
+  name: string;
+  description: string;
+  keywords: string[];
+  location: string;
+  privacy: 'Open' | 'Controlled' | 'Registered Access';
+  license: 'CC BY 4.0' | 'CC0 1.0 Universal' | 'CC BY-NC-SA 4.0';
+  lastUpdated: string; // e.g., "June 2024"
+  version: string;
+  datasets: Dataset[];
+};
+
+type ProjectsData = {
+  projects: Project[];
+};
+
+const { projects } = demoData as ProjectsData;
 
 const Catalogue = () => {
   return (
