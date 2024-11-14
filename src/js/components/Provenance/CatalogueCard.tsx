c0d92c8593365b1c041082c697834e1e4cf61487
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2024-11-14 13:12:18 -0500

    Added descriptions

--- src/js/components/Provenance/CatalogueCard.tsx
+++ src/js/components/Provenance/CatalogueCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Flex, Space, Tag, Typography } from 'antd';
+import { Card, Descriptions, Flex, Space, Tag, Typography } from 'antd';
 import { Project } from '@/components/Provenance/Catalogue';
 
 const MAX_KEYWORDS = 3;
@@ -8,12 +8,21 @@ const CatalogueCard = ({ project }: { project: Project }) => {
   const keywords = project.keywords.slice(0, MAX_KEYWORDS);
   const extraKeywordCount = Math.max(project.keywords.length - MAX_KEYWORDS, 0);
 
+  const projectInfo = [
+    { key: '1', label: 'location', children: project.location, span: 1.5 },
+    { key: '2', label: 'Privacy', children: project.privacy, span: 1.5 },
+    { key: '3', label: 'License', children: project.license, span: 1.5 },
+    { key: '4', label: 'Updated', children: `${project.lastUpdated}, v${project.version}`, span: 1.5 },
+  ];
+
   return (
     <Card key={project.name}>
       <Flex justify="space-between">
         <div style={{ width: '50%' }}>
           <Space direction="vertical">
-            <Typography.Title level={4}>{project.name}</Typography.Title>
+            <Typography.Title level={5} style={{ marginTop: 0 }}>
+              {project.name}
+            </Typography.Title>
             <Typography.Text>{project.description}</Typography.Text>
             <div>
               {keywords.map((kw) => (
@@ -22,6 +31,7 @@ const CatalogueCard = ({ project }: { project: Project }) => {
                 </Tag>
               ))}
               {extraKeywordCount !== 0 && <Typography.Text>+{extraKeywordCount} more</Typography.Text>}
+              <Descriptions items={projectInfo} />
             </div>
           </Space>
         </div>
