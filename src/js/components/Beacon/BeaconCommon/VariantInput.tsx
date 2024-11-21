d8e693bcbcc6451d787da4f863c3a5277727c388
David Lougheed <david.lougheed@gmail.com>
2024-11-21 11:46:19 -0500

    chore: rm needless <div>

--- src/js/components/Beacon/BeaconCommon/VariantInput.tsx
+++ src/js/components/Beacon/BeaconCommon/VariantInput.tsx
@@ -5,11 +5,11 @@ import type { FormField } from '@/types/beacon';
 const VariantInput = ({ field, disabled }: VariantInputProps) => {
   const t = useTranslationFn();
   return (
-    <div>
+    <>
       <Form.Item name={field.name} label={t(field.name)} rules={field.rules}>
         <Input placeholder={field.placeholder} disabled={disabled} />
       </Form.Item>
-    </div>
+    </>
   );
 };
 
