0a948f1bb98fc9ebee796a9caa9991f0d5a118d3
David Lougheed <david.lougheed@gmail.com>
2024-11-22 14:39:56 -0500

    chore: document hook

--- src/js/hooks/censorship.ts
+++ src/js/hooks/censorship.ts
@@ -8,5 +8,8 @@ export const useCanSeeUncensoredCounts = () => {
   const { hasPermission: queryDataPerm } = useHasResourcePermissionWrapper(scopeResource, queryData);
   const { countThreshold } = useConfig();
 
+  // Used mostly for UI - showing dashes vs "0".
+  //  - If we have query:data permissions or the low cell count threshold is low enough that we get uncensored counts,
+  //    then this becomes true.
   return queryDataPerm || countThreshold <= 1;
 };
