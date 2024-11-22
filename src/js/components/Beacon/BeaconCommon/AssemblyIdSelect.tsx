a581f382e8a3bf6d2d3446fcd8f98b17cebea168
David Lougheed <david.lougheed@gmail.com>
2024-11-22 11:07:35 -0500

    refact(beacon): use new select options format

--- src/js/components/Beacon/BeaconCommon/AssemblyIdSelect.tsx
+++ src/js/components/Beacon/BeaconCommon/AssemblyIdSelect.tsx
@@ -4,17 +4,11 @@ import type { FormField, BeaconAssemblyIds } from '@/types/beacon';
 
 const AssemblyIdSelect = ({ field, beaconAssemblyIds, disabled }: AssemblyIdSelectProps) => {
   const t = useTranslationFn();
-  const assemblyIdOptions = beaconAssemblyIds.map((assembly) => (
-    <Select.Option key={assembly} value={assembly}>
-      {assembly}
-    </Select.Option>
-  ));
+  const assemblyIdOptions = beaconAssemblyIds.map((assembly) => ({ value: assembly, label: assembly }));
 
   return (
     <Form.Item name={field.name} label={t(field.name)} rules={field.rules}>
-      <Select style={{ width: '100%' }} disabled={disabled}>
-        {assemblyIdOptions}
-      </Select>
+      <Select style={{ width: '100%' }} disabled={disabled} options={assemblyIdOptions} />
     </Form.Item>
   );
 };
