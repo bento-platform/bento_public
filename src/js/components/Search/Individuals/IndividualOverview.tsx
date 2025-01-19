fb78976b7ad45edd1eb582757bbc54e9d11979eb
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-01-19 18:49:52 -0500

    Extracted Individual Overview to its own component

---
+++ src/js/components/Search/Individuals/IndividualOverview.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Descriptions, DescriptionsProps } from 'antd';
+import { EM_DASH } from '@/constants/contentConstants';
+import { IndividualRootObject } from '@/types/individual';
+
+const IndividualOverivew = ({ individualData }: { individualData: IndividualRootObject }) => {
+  const items: DescriptionsProps['items'] = [
+    {
+      key: 1,
+      label: 'Date of Birth',
+      children: individualData?.date_of_birth || EM_DASH,
+      span: 2,
+    },
+    {
+      key: 2,
+      label: 'Sex',
+      children: individualData?.sex,
+    },
+    {
+      key: 3,
+      label: 'time at last encounter',
+      children: individualData?.time_at_last_encounter?.age.iso8601duration, // TODO: handle other time elements if there are any
+    },
+    {
+      key: 4,
+      label: 'Karyotypic Sex',
+      children: individualData?.karyotypic_sex || 'UNKNOWN_KARYOTYPE',
+    },
+    {
+      key: 5,
+      label: 'Taxonomy',
+      children: individualData?.taxonomy?.label || 'UNKNOWN_TAXONOMY',
+    },
+    {
+      key: 6,
+      span: 3,
+      label: 'Extra Properties',
+      children: JSON.stringify(individualData?.extra_properties, null, 2),
+    },
+  ];
+
+  return <Descriptions items={items} />;
+};
+
+export default IndividualOverivew;
--- src/js/components/Search/IndividualsAccordianPane.tsx
+++ src/js/components/Search/IndividualsAccordianPane.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
 import { makeGetIndividualData } from '@/features/search/makeGetIndividualData.thunk';
 import { useAppDispatch, useAppSelector } from '@/hooks';
-import type { DescriptionsProps } from 'antd';
-import { Descriptions } from 'antd';
-import { EM_DASH } from '@/constants/contentConstants';
+import IndividualOverivew from '@/components/Search/Individuals/IndividualOverview';
 
 const IndividualsAccordianPane = ({ id }: { id: string }) => {
   const dispatch = useAppDispatch();
@@ -13,42 +11,7 @@ const IndividualsAccordianPane = ({ id }: { id: string }) => {
 
   const individualData = useAppSelector((state) => state.query.individualDataCache[id]);
 
-  const items: DescriptionsProps['items'] = [
-    {
-      key: 1,
-      label: 'Date of Birth',
-      children: individualData?.date_of_birth || EM_DASH,
-      span: 2,
-    },
-    {
-      key: 2,
-      label: 'Sex',
-      children: individualData?.sex,
-    },
-    {
-      key: 3,
-      label: 'time at last encounter',
-      children: individualData?.time_at_last_encounter?.age.iso8601duration, // TODO: handle other time elements if there are any
-    },
-    {
-      key: 4,
-      label: 'Karyotypic Sex',
-      children: individualData?.karyotypic_sex || 'UNKNOWN_KARYOTYPE',
-    },
-    {
-      key: 5,
-      label: 'Taxonomy',
-      children: individualData?.taxonomy?.label || 'UNKNOWN_TAXONOMY',
-    },
-    {
-      key: 6,
-      span: 3,
-      label: 'Extra Properties',
-      children: JSON.stringify(individualData?.extra_properties, null, 2),
-    },
-  ];
-
-  return <Descriptions items={items} />;
+  return <IndividualOverivew individualData={individualData} />;
 };
 
 export default IndividualsAccordianPane;
