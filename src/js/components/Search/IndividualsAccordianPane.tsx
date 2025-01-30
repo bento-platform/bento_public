04b853c2bd8a337cf6aff5537fea74934d1b6c41
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-01-30 01:43:05 -0500

    Removed unnecessary keys

--- src/js/components/Search/IndividualsAccordianPane.tsx
+++ src/js/components/Search/IndividualsAccordianPane.tsx
@@ -15,13 +15,11 @@ const IndividualsAccordianPane = ({ id }: { id: string }) => {
 
   const items: DescriptionsProps['items'] = [
     {
-      key: 1,
       label: 'Date of Birth',
       children: individualData?.date_of_birth || EM_DASH,
       span: 2,
     },
     {
-      key: 2,
       label: 'Sex',
       children: individualData?.sex,
     },
@@ -30,17 +28,14 @@ const IndividualsAccordianPane = ({ id }: { id: string }) => {
       children: individualData?.time_at_last_encounter?.age.iso8601duration, // TODO: handle other time elements if there are any
     },
     {
-      key: 4,
       label: 'Karyotypic Sex',
       children: individualData?.karyotypic_sex || 'UNKNOWN_KARYOTYPE',
     },
     {
-      key: 5,
       label: 'Taxonomy',
       children: individualData?.taxonomy?.label || 'UNKNOWN_TAXONOMY',
     },
     {
-      key: 6,
       span: 3,
       label: 'Extra Properties',
       children: JSON.stringify(individualData?.extra_properties, null, 2),
