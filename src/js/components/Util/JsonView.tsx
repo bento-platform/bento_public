61d488bb0998a2d9e9027a8c6f602de4c495fea2
David Lougheed <david.lougheed@gmail.com>
2025-02-11 15:20:51 -0500

    style: port over react18-json-view styles from bento_web

--- src/js/components/Util/JsonView.tsx
+++ src/js/components/Util/JsonView.tsx
@@ -2,6 +2,8 @@ import ReactJson from 'react18-json-view';
 import type { Collapsed } from 'react18-json-view/dist/types';
 import type { JSONType } from '@/types/json';
 
+// Styles for this component are set in the Bento Public styles.css right now
+
 type JsonViewProps = {
   src: JSONType | JSONType[] | Record<string, JSONType>;
   collapsed?: Collapsed;
