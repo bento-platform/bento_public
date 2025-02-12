6f0a252493c153561747521d70aa8c3291fd42ae
David Lougheed <david.lougheed@gmail.com>
2025-02-12 08:18:39 -0500

    chore: fix comment wording

--- src/js/components/Overview/OverviewChartDashboard.tsx
+++ src/js/components/Overview/OverviewChartDashboard.tsx
@@ -22,7 +22,7 @@ import { useData, useSearchableFields } from '@/features/data/hooks';
 import { useSelectedProject, useSelectedScope } from '@/features/metadata/hooks';
 import { useSmallScreen } from '@/hooks/useResponsiveContext';
 
-// right position set based on small screen status
+// The 'right' position will be set based on small screen status dynamically
 const MANAGE_CHARTS_BUTTON_STYLE: CSSProperties = { bottom: '1.5em', transform: 'scale(125%)' };
 
 const saveToLocalStorage = (sections: Sections) => {
