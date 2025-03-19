8ec2390d85c1de3bb6db9f41a0d5e7693b2d9b01
David Lougheed <david.lougheed@gmail.com>
2025-03-19 13:37:47 -0400

    refact(overview): last ingestion loading skeleton + refact

--- src/js/components/Overview/LastIngestion.tsx
+++ src/js/components/Overview/LastIngestion.tsx
@@ -1,37 +1,76 @@
-import { useCallback } from 'react';
-import { Card, Empty, Space, Typography } from 'antd';
+import { useMemo } from 'react';
+import { Card, Empty, Skeleton, Space, Typography } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 
 import { T_PLURAL_COUNT } from '@/constants/i18n';
+import { WAITING_STATES } from '@/constants/requests';
 import { BOX_SHADOW } from '@/constants/overviewConstants';
 import { useDataTypes } from '@/features/dataTypes/hooks';
+import type { BentoServiceDataType } from '@/types/dataTypes';
+import { useTranslationFn } from '@/hooks';
+
+const formatDate = (dateString: string, language: string) => {
+  const date = new Date(dateString);
+  return !isNaN(date.getTime())
+    ? date.toLocaleString(language, {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true,
+      })
+    : 'Invalid Date';
+};
+
+const LastIngestionSkeleton = () => (
+  <Card style={BOX_SHADOW}>
+    <Space direction="vertical">
+      <Typography.Text style={{ color: 'rgba(0,0,0,0.45)' }}>
+        <Skeleton active={true} title={{ width: 100, style: { margin: 0 } }} paragraph={false} />
+      </Typography.Text>
+      <Typography.Text>
+        <CalendarOutlined />{' '}
+        <Skeleton
+          active={true}
+          style={{ display: 'inline', verticalAlign: 'top' }}
+          title={{ width: 82, style: { margin: '5px 0 0 0' } }}
+          paragraph={false}
+        />
+      </Typography.Text>
+    </Space>
+  </Card>
+);
+
+const LastIngestionDataType = ({ dataType }: { dataType: BentoServiceDataType }) => {
+  const { t, i18n } = useTranslation();
+  return (
+    <Card style={BOX_SHADOW} key={dataType.id}>
+      <Space direction="vertical">
+        <Typography.Text style={{ color: 'rgba(0,0,0,0.45)' }}>
+          {t(`entities.${dataType.id}`, T_PLURAL_COUNT)}
+        </Typography.Text>
+        <Typography.Text>
+          <CalendarOutlined />{' '}
+          {dataType.last_ingested ? formatDate(dataType.last_ingested, i18n.language) : t('Not Available')}
+        </Typography.Text>
+      </Space>
+    </Card>
+  );
+};
 
 const LastIngestionInfo = () => {
-  const { t, i18n } = useTranslation();
+  const t = useTranslationFn();
 
-  const { dataTypesById } = useDataTypes();
+  const { status: dataTypesStatus, dataTypesById } = useDataTypes();
 
-  const sortedDataTypes = Object.values(dataTypesById).sort((a, b) => a.label.localeCompare(b.label));
-
-  // Filter out the queryable data types
-  const queryableDataTypes = sortedDataTypes.filter((dataType) => dataType.queryable);
-
-  const formatDate = useCallback(
-    (dateString: string) => {
-      const date = new Date(dateString);
-      return !isNaN(date.getTime())
-        ? date.toLocaleString(i18n.language, {
-            year: 'numeric',
-            month: 'long',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: true,
-          })
-        : 'Invalid Date';
-    },
-    [i18n.language]
+  const queryableDataTypes = useMemo(
+    () =>
+      Object.values(dataTypesById)
+        .sort((a, b) => a.label.localeCompare(b.label))
+        .filter((dataType) => dataType.queryable), // Filter to only include the queryable data types
+    [dataTypesById]
   );
 
   const hasData = queryableDataTypes.length > 0;
@@ -40,20 +79,10 @@ const LastIngestionInfo = () => {
     <Space direction="vertical" size={0}>
       <Typography.Title level={3}>{t('Latest Data Ingestion')}</Typography.Title>
       <Space wrap>
-        {hasData ? (
-          queryableDataTypes.map((dataType) => (
-            <Card style={BOX_SHADOW} key={dataType.id}>
-              <Space direction="vertical">
-                <Typography.Text style={{ color: 'rgba(0,0,0,0.45)' }}>
-                  {t(`entities.${dataType.id}`, T_PLURAL_COUNT)}
-                </Typography.Text>
-                <Typography.Text>
-                  <CalendarOutlined />{' '}
-                  {dataType.last_ingested ? formatDate(dataType.last_ingested) : t('Not Available')}
-                </Typography.Text>
-              </Space>
-            </Card>
-          ))
+        {WAITING_STATES.includes(dataTypesStatus) ? (
+          <LastIngestionSkeleton />
+        ) : hasData ? (
+          queryableDataTypes.map((dataType) => <LastIngestionDataType dataType={dataType} key={dataType.id} />)
         ) : (
           <Empty description={t('Ingestion History Is Empty')} />
         )}
