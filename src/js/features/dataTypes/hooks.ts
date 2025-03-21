b614862e98ca0d49c5bb11c50b5fb149ec422b13
David Lougheed <david.lougheed@gmail.com>
2025-03-21 09:57:31 -0400

    chore: clean up useDataTypes hook effect dependencies

--- src/js/features/dataTypes/hooks.ts
+++ src/js/features/dataTypes/hooks.ts
@@ -1,18 +1,14 @@
-import { useAppDispatch, useAppSelector } from '@/hooks';
 import { useEffect } from 'react';
-import { makeGetDataTypes } from '@/features/dataTypes/dataTypes.store';
-import { useMetadata } from '@/features/metadata/hooks';
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { makeGetDataTypes } from './dataTypes.store';
 
 export const useDataTypes = () => {
   const dispatch = useAppDispatch();
   const dataTypesState = useAppSelector((state) => state.dataTypes);
-  const {
-    selectedScope: { scopeSet },
-  } = useMetadata();
 
   useEffect(() => {
     dispatch(makeGetDataTypes());
-  }, [dispatch, scopeSet, dataTypesState.isInvalid]);
+  }, [dispatch, dataTypesState.isInvalid]);
 
   return dataTypesState;
 };
