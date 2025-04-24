7bb403eb9114743ad738acbc87e2ada3fc6d753f
David Lougheed <david.lougheed@gmail.com>
2025-04-24 13:44:28 -0400

    refact(search): or divider mini component

--- src/js/components/Search/OrDelimiter.tsx
+++ src/js/components/Search/OrDelimiter.tsx
@@ -4,14 +4,20 @@ import { useTranslationFn } from '@/hooks';
 
 const lineStyle: CSSProperties = { flex: 1, backgroundColor: '#f0f0f0' };
 
+// Use a custom divider since the Ant divider comes with too many styles we have to immediately override.
+const OrDivider = memo(({ vertical }: { vertical: boolean }) => {
+  const lineStyleComputed = { ...lineStyle, ...(vertical ? { width: 1 } : { height: 1 }) };
+  return <div style={lineStyleComputed} aria-hidden={true} />;
+});
+OrDivider.displayName = 'OrDivider';
+
 const OrDelimiter = memo(({ vertical }: { vertical: boolean }) => {
   const t = useTranslationFn();
-  const lineStyleComputed = { ...lineStyle, ...(vertical ? { width: 1 } : { height: 1 }) };
   return (
     <Flex vertical={vertical} gap={8} align="center">
-      <div style={lineStyleComputed} />
+      <OrDivider vertical={vertical} />
       <div style={{ fontWeight: 'bold', color: '#595959' }}>{t('OR')}</div>
-      <div style={lineStyleComputed} />
+      <OrDivider vertical={vertical} />
     </Flex>
   );
 });
