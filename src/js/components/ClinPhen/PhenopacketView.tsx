4ee6feff3d8b6f92d261166fba09904854339171
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-04-28 13:22:19 -0400

    feat(Phenopacket): implement phenopacket data fetching and caching

--- src/js/components/ClinPhen/PhenopacketView.tsx
+++ src/js/components/ClinPhen/PhenopacketView.tsx
@@ -1,5 +1,10 @@
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Loader from '@/components/Loader';
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { makeGetPhenopacketData } from '@/features/clinPhen/makeGetPhenopacket.thunk';
 import { Card } from 'antd';
+import { RequestStatus } from '@/types/requests';
 
 interface RouteParams {
   packetId: string;
@@ -8,10 +13,23 @@ interface RouteParams {
 
 const PhenopacketView = () => {
   const { packetId } = useParams<RouteParams>();
+  const dispatch = useAppDispatch();
+  const phenopacket = useAppSelector((state) => state.clinPhen.phenopacketDataCache[packetId ?? '']);
+  const status = useAppSelector((state) => state.clinPhen.phenopacketDataStatus[packetId ?? '']);
+
+  useEffect(() => {
+    if (packetId && !phenopacket && status !== RequestStatus.Pending) {
+      dispatch(makeGetPhenopacketData(packetId));
+    }
+  }, [packetId, phenopacket, status, dispatch]);
+
+  if (status === RequestStatus.Pending || !phenopacket) {
+    return <Loader fullHeight={true} />;
+  }
 
   return (
     <Card title={packetId}>
-      <p>Packet ID: {packetId}</p>
+      <pre>{JSON.stringify(phenopacket, null, 2)}</pre>
     </Card>
   );
 };
---
+++ src/js/features/clinPhen/makeGetPhenopacket.thunk.ts
@@ -0,0 +1,37 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { phenopacketUrl } from '@/constants/configConstants';
+import type { RootState } from '@/store';
+import { printAPIError } from '@/utils/error.util';
+import { authorizedRequestConfig } from '@/utils/requests';
+import type { Phenopacket } from '@/types/clinPhen/phenopacket';
+import { RequestStatus } from '@/types/requests';
+
+export const makeGetPhenopacketData = createAsyncThunk<
+  Phenopacket,
+  string,
+  {
+    state: RootState;
+    rejectValue: string;
+  }
+>(
+  'query/makeGetPhenopacketData',
+  (id, { rejectWithValue, getState }) => {
+    return axios
+      .get(`${phenopacketUrl}/${id}`, authorizedRequestConfig(getState()))
+      .then((res) => res.data)
+      .catch(printAPIError(rejectWithValue));
+  },
+  {
+    condition(id, { getState }) {
+      const state = getState().clinPhen;
+      const cond = !state.phenopacketDataCache[id] && state.phenopacketDataStatus[id] !== RequestStatus.Pending;
+      console.debug(
+        ...(cond
+          ? ['requesting phenopacket data for id', id]
+          : ['not requesting phenopacket data for id', id, '- already fetched or fetching'])
+      );
+      return cond;
+    },
+  }
+);
