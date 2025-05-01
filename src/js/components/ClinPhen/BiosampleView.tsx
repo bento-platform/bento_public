41ae8697aeb9d09219148f516da41a35204c4acb
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-05-01 14:09:15 -0400

    feat(PhenopacketView): integrate BiosampleView component and enhance tab structure for phenopacket details

---
+++ src/js/components/ClinPhen/BiosampleView.tsx
@@ -0,0 +1,72 @@
+import { Descriptions, DescriptionsProps, Table } from 'antd';
+import { Biosample } from '@/types/clinPhen/biosample';
+import { OntologyTerm } from '@/types/ontology';
+
+const BiosampleExpandedRow: React.FC<{ biosample: Biosample }> = ({ biosample }) => {
+  const items: DescriptionsProps['items'] = [
+    {
+      key: 'description',
+      label: 'Description',
+      children: biosample.description || 'N/A',
+    },
+    {
+      key: 'derived_from_id',
+      label: 'Derived From',
+      children: biosample.derived_from_id || 'N/A',
+    },
+    {
+      key: 'sample_type',
+      label: 'Sample Type',
+      children: biosample.sample_type?.label || 'N/A',
+    },
+    {
+      key: 'time_of_collection',
+      label: 'Collection Time',
+      children: biosample.time_of_collection ? JSON.stringify(biosample.time_of_collection) : 'N/A',
+    },
+    {
+      key: 'histological_diagnosis',
+      label: 'Histological Diagnosis',
+      children: biosample.histological_diagnosis?.label || 'N/A',
+    },
+    {
+      key: 'pathological_stage',
+      label: 'Pathological Stage',
+      children: biosample.pathological_stage?.label || 'N/A',
+    },
+  ];
+
+  return <Descriptions bordered size="small" items={items} />;
+};
+
+interface BiosampleViewProps {
+  biosamples: Biosample[];
+  handleExperimentClick: (experimentId: string) => void;
+}
+
+const BiosampleView: React.FC<BiosampleViewProps> = ({ biosamples, handleExperimentClick }) => {
+  const columns = [
+    {
+      title: 'Biosample ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Sampled Tissue',
+      dataIndex: 'sampled_tissue',
+      key: 'sampled_tissue',
+      render: (text: OntologyTerm) => text?.label || 'N/A',
+    },
+  ];
+  return (
+    <Table<Biosample>
+      dataSource={biosamples}
+      columns={columns}
+      expandable={{
+        expandedRowRender: (record) => <BiosampleExpandedRow biosample={record} />,
+      }}
+    />
+  );
+};
+
+export default BiosampleView;
--- src/js/components/ClinPhen/PhenopacketView.tsx
+++ src/js/components/ClinPhen/PhenopacketView.tsx
@@ -3,8 +3,17 @@ import { useParams } from 'react-router-dom';
 import Loader from '@/components/Loader';
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { makeGetPhenopacketData } from '@/features/clinPhen/makeGetPhenopacket.thunk';
-import { Card } from 'antd';
+import { Card, Tabs, Descriptions } from 'antd';
 import { RequestStatus } from '@/types/requests';
+import BiosampleView from './BiosampleView';
+
+const { TabPane } = Tabs;
+
+const getTabContent = (title: string, data: any) => (
+  <Descriptions title={title}>
+    <Descriptions.Item label="Data">{JSON.stringify(data, null, 2)}</Descriptions.Item>
+  </Descriptions>
+);
 
 interface RouteParams {
   packetId: string;
@@ -29,7 +38,29 @@ const PhenopacketView = () => {
 
   return (
     <Card title={packetId}>
-      <pre>{JSON.stringify(phenopacket, null, 2)}</pre>
+      <Tabs defaultActiveKey="biosamples">
+        <TabPane key="biosamples" tab="Biosamples" disabled={!phenopacket.biosamples}>
+          <BiosampleView biosamples={phenopacket.biosamples!} />
+        </TabPane>
+        <TabPane key="measurements" tab="Measurements" disabled={!phenopacket.measurements}>
+          {getTabContent('Measurements', phenopacket.measurements)}
+        </TabPane>
+        <TabPane key="phenotypic_features" tab="Phenotypic Features" disabled={!phenopacket.phenotypic_features}>
+          {getTabContent('Phenotypic Features', phenopacket.phenotypic_features)}
+        </TabPane>
+        <TabPane key="diseases" tab="Diseases" disabled={!phenopacket.diseases}>
+          {getTabContent('Diseases', phenopacket.diseases)}
+        </TabPane>
+        <TabPane key="interpretations" tab="Interpretations" disabled={!phenopacket.interpretations}>
+          {getTabContent('Interpretations', phenopacket.interpretations)}
+        </TabPane>
+        <TabPane key="medical_actions" tab="Medical Actions" disabled={!phenopacket.medical_actions}>
+          {getTabContent('Medical Actions', phenopacket.medical_actions)}
+        </TabPane>
+        {/* <TabPane key="ontologies" tab="Ontologies" disabled={!phenopacket.ontologies}>
+          {getTabContent('Ontologies', phenopacket.ontologies)}
+        </TabPane> */}
+      </Tabs>
     </Card>
   );
 };
