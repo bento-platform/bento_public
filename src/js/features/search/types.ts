9fa487e830692a0762601f36992240b88ec8ab58
David Lougheed <david.lougheed@gmail.com>
2025-05-23 13:16:37 -0400

    work on new discovery response formats + typing

--- src/js/features/search/types.ts
+++ src/js/features/search/types.ts
@@ -6,35 +6,83 @@ export type QueryParams = { [key: string]: string };
 export type QueryMode = 'filters' | 'text';
 
 export interface SearchFieldResponse {
-  sections: Section[];
+  sections: SearchFieldSection[];
 }
 
-export interface Section {
-  fields: Field[];
+export interface SearchFieldSection {
+  fields: SearchFieldAndOptions[];
   section_title: string;
 }
 
-// TODO: fix these types based on pydantic
+/* BEGIN DISCOVERY FIELD TYPE DEFINITION -------------------------------------------------------------------------------
+ *   See https://github.com/bento-platform/bento_lib/blob/master/bento_lib/discovery/models/fields.py
+ */
 
-export interface Field {
-  config: Config;
-  datatype: 'number' | 'string' | 'date';
-  description: string;
-  id: string;
-  mapping: string;
-  mapping_for_search_filter?: string;
+type BaseField = {
   title: string;
-}
+  description: string;
+  datatype: 'number' | 'string' | 'date';
+  mapping: string;
+  mapping_for_search_filter?: string | null;
+  group_by?: string | null;
+  group_by_value?: string | null;
+  value_mapping?: string | null;
+};
 
-export interface Config {
-  bin_size?: number;
-  maximum?: number;
-  minimum?: number;
-  taper_left?: number;
-  taper_right?: number;
-  units?: string;
-  enum?: string[] | null;
-}
+type BaseNumberFieldConfig = {
+  units?: string | null;
+};
+
+export type ManualBinsNumberFieldConfig = BaseNumberFieldConfig & {
+  bins: number[];
+  minimum?: number | null;
+  maximum?: number | null;
+};
+
+export type AutoBinsNumberFieldConfig = BaseNumberFieldConfig & {
+  bin_size: number;
+  taper_left: number;
+  taper_right: number;
+  minimum: number;
+  maximum: number;
+};
+
+export type NumberFieldConfig = ManualBinsNumberFieldConfig | AutoBinsNumberFieldConfig;
+
+export type NumberField = BaseField & {
+  datatype: 'number';
+  config: NumberFieldConfig;
+};
+
+export type StringFieldConfig = {
+  enum: string[] | null;
+};
+
+export type StringField = BaseField & {
+  datatype: 'string';
+  config: StringFieldConfig;
+};
+
+export type DateFieldConfig = {
+  bin_by: 'month';
+};
+
+export type DateField = BaseField & {
+  datatype: 'date';
+  config: DateFieldConfig;
+};
+
+export type Field = NumberField | StringField | DateField;
+
+// ----
+
+export type SearchFieldAndOptions = {
+  id: string;
+  definition: Field;
+  options: string[];
+};
+
+// END DISCOVERY FIELD TYPE DEFINITION ---------------------------------------------------------------------------------
 
 export type KatsuIndividualMatch = {
   id: string;
