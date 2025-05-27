99e1dacdf5c7907c7899d4784ad405ab0ea3a6fc
David Lougheed <david.lougheed@gmail.com>
2025-05-27 14:42:53 -0400

    refact(search): factor out search form into independent component

--- src/js/components/Search/Search.tsx
+++ src/js/components/Search/Search.tsx
@@ -295,34 +295,42 @@ const SEARCH_CARD_STYLES: CardProps['styles'] = {
   body: { ...CARD_BODY_STYLE, padding: '20px 24px 24px 24px' },
 };
 
-const Search = () => {
+export const SearchForm = () => {
   const dispatch = useAppDispatch();
+
   const isSmallScreen = useSmallScreen();
   const { hasPermission: queryDataPerm } = useHasScopePermission(queryData);
-  const { mode: queryMode, fieldsStatus } = useSearchQuery();
+  const { mode: queryMode } = useSearchQuery();
 
   const onFiltersFocus = useCallback(() => dispatch(setQueryMode('filters')), [dispatch]);
   const onTextFocus = useCallback(() => dispatch(setQueryMode('text')), [dispatch]);
 
+  return (
+    <Card className="w-full shadow rounded-xl" styles={SEARCH_CARD_STYLES}>
+      <Flex justify="space-between" gap={isSmallScreen ? 12 : 24} className="w-full" vertical={isSmallScreen}>
+        <SearchFilters focused={queryMode === 'filters'} onFocus={onFiltersFocus} className="max-w-half-cmw" />
+        {queryDataPerm && (
+          // If we have the query:data permission on the current scope, we're allowed to run free-text searches on
+          // the data, so show the free-text search form:
+          <>
+            <OrDelimiter vertical={!isSmallScreen} />
+            <SearchFreeText focused={queryMode === 'text'} onFocus={onTextFocus} />
+          </>
+        )}
+      </Flex>
+    </Card>
+  );
+};
+
+const Search = () => {
+  const { fieldsStatus } = useSearchQuery();
   return WAITING_STATES.includes(fieldsStatus) ? (
     <Loader />
   ) : (
     <Row justify="center">
       <Space direction="vertical" align="center" className="w-full" styles={SPACE_ITEM_WIDTH_100P_STYLES}>
         <div className="container margin-auto">
-          <Card className="w-full shadow rounded-xl" styles={SEARCH_CARD_STYLES}>
-            <Flex justify="space-between" gap={isSmallScreen ? 12 : 24} className="w-full" vertical={isSmallScreen}>
-              <SearchFilters focused={queryMode === 'filters'} onFocus={onFiltersFocus} className="max-w-half-cmw" />
-              {queryDataPerm && (
-                // If we have the query:data permission on the current scope, we're allowed to run free-text searches on
-                // the data, so show the free-text search form:
-                <>
-                  <OrDelimiter vertical={!isSmallScreen} />
-                  <SearchFreeText focused={queryMode === 'text'} onFocus={onTextFocus} />
-                </>
-              )}
-            </Flex>
-          </Card>
+          <SearchForm />
         </div>
         <SearchResults />
       </Space>
