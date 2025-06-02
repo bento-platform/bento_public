22a22a60048a784291947492f0405d710fa1d9e4
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-06-02 14:06:49 -0400

    feat: add translation keys for gene interpretation attributes in English and French

--- src/js/components/Util/ClinPhen/InterpretationUtilities.tsx
+++ src/js/components/Util/ClinPhen/InterpretationUtilities.tsx
@@ -1,4 +1,4 @@
-import { Descriptions, DescriptionsProps, Space, Table } from 'antd';
+import { type DescriptionsProps, Space, Table } from 'antd';
 
 import { VariantInterpretation as VariantInterpretationType } from '@/types/clinPhen/variantInterpretation';
 import JsonView from '../JsonView';
@@ -8,44 +8,37 @@ import OntologyTerm from './OntologyTerm';
 import { JSONType } from '@/types/json';
 import { GeneDescriptor as GeneDescriptorType } from '@/types/clinPhen/geneDescriptor';
 import StringList from '../StringList';
+import TDescriptions from '../TDescriptions';
+import { useTranslatedTableColumnTitles } from '@/hooks/useTranslatedTableColumnTitles';
 
-// TODO: add link to tracks
 export const GeneDescriptor = ({ geneDescriptor }: { geneDescriptor: GeneDescriptorType }) => {
   const items: DescriptionsProps['items'] = [
-    { key: 'id', label: 'Accession Number', children: geneDescriptor.value_id },
-    { key: 'symbol', label: 'Symbol', children: geneDescriptor.symbol }, // Link to tracks
-    {
-      key: 'description',
-      label: 'Description',
-      children: geneDescriptor.description,
-    },
+    { key: 'id', label: 'interpretations.accession_number', children: geneDescriptor.value_id },
+    { key: 'symbol', label: 'interpretations.symbol', children: geneDescriptor.symbol },
+    { key: 'description', label: 'interpretations.description', children: geneDescriptor.description },
     {
       key: 'alternate_ids',
-      label: 'Alternate IDs',
+      label: 'interpretations.alternate_ids',
       children: <StringList list={geneDescriptor.alternate_ids} />,
     },
-    {
-      key: 'xrefs',
-      label: 'Cross References',
-      children: <StringList list={geneDescriptor.xrefs} />,
-    },
+    { key: 'xrefs', label: 'interpretations.cross_references', children: <StringList list={geneDescriptor.xrefs} /> },
     {
       key: 'alternate_symbols',
-      label: 'Alternate Symbols',
+      label: 'interpretations.alternate_symbols',
       children: <StringList list={geneDescriptor.alternate_symbols} />,
     },
   ];
 
-  return <Descriptions items={items} size="small" column={1} bordered />;
+  return <TDescriptions items={items} size="small" column={1} bordered />;
 };
 
 const VariantExpressionDetails = ({ variantExpression }: { variantExpression: Expression }) => {
   const items: DescriptionsProps['items'] = [
-    { key: 'syntax', label: 'Syntax', children: variantExpression.syntax },
-    { key: 'value', label: 'Value', children: variantExpression.value },
+    { key: 'syntax', label: 'interpretations.syntax', children: variantExpression.syntax },
+    { key: 'value', label: 'interpretations.value', children: variantExpression.value },
   ];
 
-  return <Descriptions items={items} size="small" />;
+  return <TDescriptions items={items} size="small" />;
 };
 
 export const VariantInterpretation = ({
@@ -56,102 +49,90 @@ export const VariantInterpretation = ({
   const items: DescriptionsProps['items'] = [
     {
       key: 'acmg_pathogenicity_classification',
-      label: 'ACMG Pathogenicity Classification',
+      label: 'interpretations.acmg_pathogenicity_classification',
       children: variantInterpretation?.acmg_pathogenicity_classification,
     },
     {
       key: 'therapeutic_actionability',
-      label: 'Therapeutic Actionability',
+      label: 'interpretations.therapeutic_actionability',
       children: variantInterpretation?.therapeutic_actionability,
     },
   ];
 
   const vd = variantInterpretation.variation_descriptor;
 
-  const extentionTableColumns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Value',
-      dataIndex: 'value',
-      key: 'value',
-    },
-  ];
+  const extentionTableColumns = useTranslatedTableColumnTitles<Extension>([
+    { title: 'interpretations.name', dataIndex: 'name', key: 'name' },
+    { title: 'interpretations.value', dataIndex: 'value', key: 'value' },
+  ]);
 
   const variantDescrptorItems: DescriptionsProps['items'] = [
-    { key: 'id', label: 'ID', children: vd.id },
+    { key: 'id', label: 'interpretations.id', children: vd.id },
     vd?.variation && {
-      key: 'Variation',
-      label: 'Variation',
+      key: 'variation',
+      label: 'interpretations.variation',
       children: <JsonView src={vd.variation as unknown as JSONType} />,
     },
-    vd?.label && { key: 'Label', label: 'Label', children: vd.label },
-    vd?.description && { key: 'Description', label: 'Description', children: vd.description },
+    vd?.label && { key: 'label', label: 'interpretations.label', children: vd.label },
+    vd?.description && { key: 'description', label: 'interpretations.description', children: vd.description },
     vd?.gene_context && {
-      key: 'Gene Context',
-      label: 'Gene Context',
+      key: 'gene_context',
+      label: 'interpretations.gene_context',
       children: <GeneDescriptor geneDescriptor={vd.gene_context} />,
     },
     vd?.expressions && {
-      key: 'Expressions',
-      label: 'Expressions',
+      key: 'expressions',
+      label: 'interpretations.expressions',
       children: (
         <Space direction="vertical" size="small">
-          {vd.expressions.map((e) => (
-            <VariantExpressionDetails variantExpression={e} />
+          {vd.expressions.map((e, i) => (
+            <VariantExpressionDetails key={i} variantExpression={e} />
           ))}
         </Space>
       ),
     },
     vd?.vcf_record && {
-      key: 'VCF Record',
-      label: 'VCF Record',
+      key: 'vcf_record',
+      label: 'interpretations.vcf_record',
       children: <JsonView src={vd.vcf_record as unknown as JSONType} />,
     },
-    vd?.xrefs && {
-      key: 'Xrefs',
-      label: 'Xrefs',
-      children: vd.xrefs,
-    },
+    vd?.xrefs && { key: 'xrefs', label: 'interpretations.xrefs', children: vd.xrefs },
     vd?.alternate_labels && {
-      key: 'Alternate Labels',
-      label: 'Alternate Labels',
+      key: 'alternate_labels',
+      label: 'interpretations.alternate_labels',
       children: vd.alternate_labels,
     },
     vd?.extensions && {
-      key: 'Extensions',
-      label: 'Extensions',
+      key: 'extensions',
+      label: 'interpretations.extensions',
       children: <Table<Extension> columns={extentionTableColumns} dataSource={vd.extensions} />,
     },
     vd?.molecule_context && {
-      key: 'Molecule Context',
-      label: 'Molecule Context',
+      key: 'molecule_context',
+      label: 'interpretations.molecule_context',
       children: vd.molecule_context,
     },
     vd?.structural_type && {
-      key: 'Structural Type',
-      label: 'Structural Type',
+      key: 'structural_type',
+      label: 'interpretations.structural_type',
       children: <OntologyTerm term={vd.structural_type} />,
     },
     vd?.vrs_ref_allele_seq && {
-      key: 'VRS Ref Allele Seq',
-      label: 'VRS Ref Allele Seq',
+      key: 'vrs_ref_allele_seq',
+      label: 'interpretations.vrs_ref_allele_seq',
       children: vd.vrs_ref_allele_seq,
     },
     vd?.allelic_state && {
-      key: 'Allelic State',
-      label: 'Allelic State',
+      key: 'allelic_state',
+      label: 'interpretations.allelic_state',
       children: <OntologyTerm term={vd.allelic_state} />,
     },
   ].filter(Boolean) as DescriptionsProps['items'];
 
   return (
     <Space direction="vertical">
-      <Descriptions items={items} size="small" bordered />
-      <Descriptions items={variantDescrptorItems} size="small" bordered />
+      <TDescriptions items={items} size="small" bordered />
+      <TDescriptions items={variantDescrptorItems} size="small" bordered />
     </Space>
   );
 };
