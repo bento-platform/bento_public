434ac663e9221d1d25067b44711005a25e109205
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-06-02 13:51:56 -0400

    feat: add translation keys for subject attributes in English and French

--- src/js/components/ClinPhen/PhenopacketDisplay/SubjectView.tsx
+++ src/js/components/ClinPhen/PhenopacketDisplay/SubjectView.tsx
@@ -1,10 +1,10 @@
-import { Descriptions, DescriptionsProps } from 'antd';
-
 import OntologyTerm from '@Util/ClinPhen/OntologyTerm';
 import JsonView from '@Util/JsonView';
 import StringList from '@Util/StringList';
 import TimeElementDisplay from '@Util/ClinPhen/TimeElementDisplay';
+import TDescriptions from '@/components/Util/TDescriptions';
 
+import type { DescriptionsProps } from 'antd';
 import type { Individual } from '@/types/clinPhen/individual';
 
 import { EM_DASH } from '@/constants/common';
@@ -14,22 +14,22 @@ const SubjectView = ({ subject }: { subject: Individual }) => {
   const vitalStatusItems: DescriptionsProps['items'] = [
     {
       key: 'status',
-      label: 'Status',
+      label: 'subject.status',
       children: vs?.status,
     },
     {
       key: 'time_of_death',
-      label: 'Time of Death',
+      label: 'subject.time_of_death',
       children: <TimeElementDisplay element={vs?.time_of_death} />,
     },
     {
       key: 'cause_of_death',
-      label: 'Cause of Death',
+      label: 'subject.cause_of_death',
       children: <OntologyTerm term={vs?.cause_of_death} />,
     },
     {
       key: 'survival_time_in_days',
-      label: 'Survival Time in Days',
+      label: 'subject.survival_time_in_days',
       children: vs?.survival_time_in_days,
     },
   ];
@@ -37,52 +37,52 @@ const SubjectView = ({ subject }: { subject: Individual }) => {
   const items: DescriptionsProps['items'] = [
     {
       key: 'id',
-      label: 'ID',
+      label: 'subject.id',
       children: subject.id,
     },
     {
       key: 'alternate_ids',
-      label: 'Alternate IDs',
+      label: 'subject.alternate_ids',
       children: <StringList list={subject.alternate_ids} />,
     },
     {
       key: 'age',
-      label: 'Age',
+      label: 'subject.age',
       children: `${subject?.age_numeric} ${subject?.age_unit}`,
     },
     {
       key: 'date_of_birth',
-      label: 'Date of Birth',
+      label: 'subject.date_of_birth',
       children: subject?.date_of_birth,
     },
     {
       key: 'time_at_last_encounter',
-      label: 'Time at Last Encounter',
+      label: 'subject.time_at_last_encounter',
       children: <TimeElementDisplay element={subject?.time_at_last_encounter} />,
     },
     {
       key: 'vital_status',
-      label: 'Vital Status',
-      children: subject?.vital_status ? <Descriptions items={vitalStatusItems} /> : EM_DASH, //todo: complete
+      label: 'subject.vital_status',
+      children: subject?.vital_status ? <TDescriptions items={vitalStatusItems} /> : EM_DASH,
     },
     {
       key: 'sex',
-      label: 'Sex',
+      label: 'subject.sex',
       children: subject?.sex,
     },
     {
       key: 'karyotypic_sex',
-      label: 'Karyotypic_sex',
+      label: 'subject.karyotypic_sex',
       children: subject?.karyotypic_sex,
     },
     {
       key: 'taxonomy',
-      label: 'Taxonomy',
+      label: 'subject.taxonomy',
       children: <OntologyTerm term={subject?.taxonomy} />,
     },
     {
       key: 'gender',
-      label: 'gender',
+      label: 'subject.gender',
       children: <OntologyTerm term={subject?.gender} />,
     },
   ];
@@ -97,7 +97,7 @@ const SubjectView = ({ subject }: { subject: Individual }) => {
     items.push(...extraProperties);
   }
 
-  return <Descriptions items={items} column={1} bordered />;
+  return <TDescriptions items={items} column={1} bordered />;
 };
 
 export default SubjectView;
