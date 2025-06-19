528aaf155d78a5a85e62a2fceeee31b25843313e
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-06-19 11:18:05 -0400

    refactor: change import statement to use 'import type' for JSONType in boolean utility

--- src/js/utils/boolean.ts
+++ src/js/utils/boolean.ts
@@ -1,4 +1,4 @@
-import { JSONType } from '@/types/json';
+import type { JSONType } from '@/types/json';
 
 export const objectToBoolean = (obj: object | undefined | JSONType): boolean => {
   return obj ? Object.keys(obj).length > 0 : false;
