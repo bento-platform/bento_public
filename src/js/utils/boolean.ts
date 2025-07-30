76345d2b302e0cc1bc60856da8b25f4febd1006b
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-07-30 14:48:25 -0400

    simplified function decs

--- src/js/utils/boolean.ts
+++ src/js/utils/boolean.ts
@@ -1,5 +1,4 @@
 import type { JSONType } from '@/types/json';
 
-export const objectToBoolean = (obj: object | undefined | JSONType): boolean => {
-  return obj ? Object.keys(obj).length > 0 : false;
-};
+export const objectToBoolean = (obj: object | undefined | JSONType): boolean =>
+  obj ? Object.keys(obj).length > 0 : false;
--- src/js/utils/tables.ts
+++ src/js/utils/tables.ts
@@ -1,8 +1,7 @@
 import type { WithVisible } from '@/types/util';
 
-export const addVisibilityProperty = <T>(r: T[], visibilityFunc: (r: T) => boolean): WithVisible<T>[] => {
-  return r.map((item) => ({
+export const addVisibilityProperty = <T>(r: T[], visibilityFunc: (r: T) => boolean): WithVisible<T>[] =>
+  r.map((item) => ({
     ...item,
     isVisible: visibilityFunc(item),
   }));
-};
