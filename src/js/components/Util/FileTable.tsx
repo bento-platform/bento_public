f750d2ba9a53cf638b2d50df53546f00e4db9b4a
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-08-18 14:33:58 -0400

    added a file list component

---
+++ src/js/components/Util/FileTable.tsx
@@ -0,0 +1,33 @@
+import { Button, Table } from 'antd';
+import { File } from '@/types/clinPhen/file';
+import { useTranslatedTableColumnTitles } from '@/hooks/useTranslatedTableColumnTitles';
+import ExtraProperties from './ExtraProperties';
+import { EM_DASH } from '@/constants/common';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const FileTable = ({ files }: { files: File[] }) => {
+  const columns = useTranslatedTableColumnTitles<File>([
+    {
+      title: 'file.download',
+      dataIndex: 'uri',
+      render: (uri: string) => (
+        <a href={uri} target="_blank">
+          <Button icon={<DownloadOutlined />} />
+        </a>
+      ),
+    },
+    {
+      title: 'file.individual_to_file_identifiers',
+      dataIndex: 'individual_to_file_identifiers',
+      render: (d: File['individual_to_file_identifiers']) => (d ? <ExtraProperties extraProperties={d} /> : EM_DASH),
+    },
+    {
+      title: 'file.file_attributes',
+      dataIndex: 'file_attributes',
+      render: (d: File['file_attributes']) => (d ? <ExtraProperties extraProperties={d} /> : EM_DASH),
+    },
+  ]);
+  return <Table<File> columns={columns} />;
+};
+
+export default FileTable;
