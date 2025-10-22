3f6eabf6220f108eed2c8da605a3063480f98e20
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-10-22 13:55:44 -0400

    used url to retain collapse state

--- src/js/components/ClinPhen/PhenopacketDisplay/CompactView.tsx
+++ src/js/components/ClinPhen/PhenopacketDisplay/CompactView.tsx
@@ -1,14 +1,35 @@
-import { Collapse, Typography } from 'antd';
+import { Collapse } from 'antd';
 import { Phenopacket } from '@/types/clinPhen/phenopacket';
 import { sectionSpecs, SectionKey } from './compactView.registry';
+import { useSearchParams } from 'react-router-dom';
 
-const { Title } = Typography;
+const qk = 'collapse';
+
+const seriazlizeKeys = (keys: SectionKey[], prev: URLSearchParams | null = null): URLSearchParams => {
+  const keyString = keys.join(',');
+  const previous = prev ? prev.toString() : '';
+  const returnVal = new URLSearchParams(previous);
+  returnVal.set(qk, keyString);
+  return returnVal;
+};
+
+const deseriazlizeKeys = (params: URLSearchParams): SectionKey[] => {
+  const queryVals = params.get(qk);
+  const keyArray = queryVals?.split(',') as SectionKey[];
+  return keyArray;
+};
 
 interface CompactViewProps {
   phenopacket: Phenopacket;
 }
 
 const CompactView = ({ phenopacket }: CompactViewProps) => {
+  const [open, setOpen] = useSearchParams(seriazlizeKeys(['subject']));
+
+  const handleCollapseChange = (e: string[]) => {
+    setOpen(seriazlizeKeys(e as SectionKey[], open));
+  };
+
   const sections = Object.entries(sectionSpecs).sort((a, b) => (a[1].order ?? 0) - (b[1].order ?? 0)) as [
     SectionKey,
     (typeof sectionSpecs)[SectionKey],
