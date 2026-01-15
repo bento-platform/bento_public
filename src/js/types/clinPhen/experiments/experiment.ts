9da31954fa9019c06f97067f5d0f9972bd34871d
noctillion <ornatus30@gmail.com>
2026-01-15 14:45:40 -0500

    update experiment interface to align to ga4gh

--- src/js/types/clinPhen/experiments/experiment.ts
+++ src/js/types/clinPhen/experiments/experiment.ts
@@ -52,6 +52,13 @@ export interface Experiment extends ExtraPropertiesEntity {
     | 'Other';
   library_selection?: 'Random' | 'PCR' | 'Random PCR' | 'RT-PCR' | 'MF' | 'Exome capture' | 'Other';
   library_layout?: 'Single' | 'Paired';
+  library_id?: string;
+  library_extract_id?: string;
+  insert_size?: number;
+  description?: string;
+  library_description?: string;
+  protocol_url?: string;
+  ga4gh_template_id?: string;
   extraction_protocol?: string;
   reference_registry_id?: string;
   qc_flags?: string[];
