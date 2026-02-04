d679b628f070b95aacd644f8946769f60850347c
David Lougheed <david.lougheed@gmail.com>
2026-02-04 15:45:37 -0500

    only render ontology class if free text == ontology class label

--- src/js/components/Util/ClinPhen/FreeTextAndOrOntologyClass.tsx
+++ src/js/components/Util/ClinPhen/FreeTextAndOrOntologyClass.tsx
@@ -4,7 +4,13 @@ import OntologyTermComponent from '@Util/ClinPhen/OntologyTerm';
 
 const FreeTextAndOrOntologyClass = ({ text, ontologyClass }: { text?: string; ontologyClass?: OntologyTerm }) => {
   const t = useTranslationFn();
-  return text ? (
+  // Cases:
+  //  - only text --> render text
+  //  - only ontology class --> render ontology class
+  //  - both -->
+  //      if text == ontology class label, only render ontology class
+  //      otherwise, render both
+  return text && (!ontologyClass || text !== ontologyClass.label) ? (
     ontologyClass ? (
       <>
         {text} ({t('general.ontology_class')}: <OntologyTermComponent term={ontologyClass} />)
