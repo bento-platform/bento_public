4d3bc36aa6fa89303f7579426134f9a57fe3af8f
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2026-02-17 11:05:38 -0500

    feat(InteractableText): Added optional extra class names

--- src/js/components/Util/InteractableText.tsx
+++ src/js/components/Util/InteractableText.tsx
@@ -1,11 +1,12 @@
 import { Typography } from 'antd';
 import { type TextProps } from 'antd/es/typography/Text';
+import clsx from 'clsx/clsx';
 
 const { Text } = Typography;
 
-const InteractableText = ({ children, ...props }: TextProps) => {
+const InteractableText = ({ children, className, ...props }: TextProps) => {
   return (
-    <Text className="dotted-underline" {...props}>
+    <Text className={clsx("dotted-underline", className)} {...props}>
       {children}
     </Text>
   );
