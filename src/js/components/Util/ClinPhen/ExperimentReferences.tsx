c1689c0b090530046b6a7959f9386019686ad967
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2026-02-23 16:14:23 -0500

    feat; Added speciali handling for "Other"

--- src/js/components/Util/ClinPhen/ExperimentReferences.tsx
+++ src/js/components/Util/ClinPhen/ExperimentReferences.tsx
@@ -4,19 +4,22 @@ import { Tooltip } from 'antd';
 import PhenopacketLink from '@/components/ClinPhen/PhenopacketLink';
 
 import type { Experiment } from '@/types/clinPhen/experiments/experiment';
+import OntologyTerm from './OntologyTerm';
 
 const ExperimentReferences = ({
   packetId,
   experiments,
 }: {
   packetId?: string;
-  experiments: Pick<Experiment, 'experiment_type' | 'id'>[];
+  experiments: Pick<Experiment, 'experiment_type' | 'id' | 'experiment_ontology'>[];
 }) => {
   const typeSafeExperiments = experiments ? [...experiments] : [];
   const sorted = typeSafeExperiments
     .sort((a, b) => a.experiment_type.localeCompare(b.experiment_type) || a.id.localeCompare(b.id))
     .map((e, _, arr) => ({
       ...e,
+      displayType:
+        e.experiment_type === 'Other' && e.experiment_ontology?.label ? e.experiment_ontology.label : e.experiment_type,
       typeIndex: arr.slice(0, arr.indexOf(e)).filter((x) => x.experiment_type === e.experiment_type).length + 1,
       hasDuplicates: arr.filter((x) => x.experiment_type === e.experiment_type).length > 1,
     }));
@@ -26,7 +29,8 @@ const ExperimentReferences = ({
         <Fragment key={e.id}>
           <PhenopacketLink.Experiment packetId={packetId} experimentId={e.id}>
             <Tooltip title={e.id} styles={{ body: { wordWrap: 'normal', inlineSize: 'max-content' } }}>
-              {e.experiment_type}{e.hasDuplicates ? ` (${e.typeIndex})` : ''}
+              {e.displayType}
+              {e.hasDuplicates ? ` (${e.typeIndex})` : ''}
             </Tooltip>
           </PhenopacketLink.Experiment>
 
