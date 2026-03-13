fbe0c0114ea4bad2dd074cf36197d6f82db73fc1
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2026-03-13 00:22:10 -0400

    feat: Added popover on experiment hover

--- src/js/components/Util/ClinPhen/ExperimentReferences.tsx
+++ src/js/components/Util/ClinPhen/ExperimentReferences.tsx
@@ -1,11 +1,29 @@
 import { Fragment } from 'react';
-import { Tooltip } from 'antd';
+import { Popover } from 'antd';
 
 import PhenopacketLink from '@/components/ClinPhen/PhenopacketLink';
+import TDescriptions from '@Util/TDescriptions';
+import OntologyTerm from './OntologyTerm';
+import FreeTextAndOrOntologyClass from './FreeTextAndOrOntologyClass';
 
+import type { ConditionalDescriptionItem } from '@/types/descriptions';
 import type { Experiment } from '@/types/clinPhen/experiments/experiment';
 import type { MatchIncludedFieldsFromExperiments } from '@/features/search/types';
 import { useTranslationFn } from '@/hooks';
+
+const ExperimentPopoverContent = ({ e }: { e: Pick<Experiment, MatchIncludedFieldsFromExperiments> }) => {
+  const items: ConditionalDescriptionItem[] = [
+    { key: 'experiment_id', children: e.id },
+    { key: 'description', children: e.description },
+    { key: 'study_type', children: e.study_type },
+    {
+      key: 'molecule',
+      children: <FreeTextAndOrOntologyClass text={e.molecule} ontologyClass={e.molecule_ontology} />,
+      isVisible: e?.molecule_ontology || e?.molecule,
+    },
+  ];
+  return <TDescriptions column={1} items={items} size="compact" defaultI18nPrefix="experiment." bordered />;
+};
 
 const ExperimentReferences = ({
   packetId,
@@ -26,16 +44,18 @@ const ExperimentReferences = ({
         e.experiment_type === 'Other' && e.experiment_ontology?.label ? e.experiment_ontology.label : e.experiment_type,
       typeIndex: arr.slice(0, arr.indexOf(e)).filter((x) => x.experiment_type === e.experiment_type).length + 1,
       hasDuplicates: arr.filter((x) => x.experiment_type === e.experiment_type).length > 1,
+      content: <ExperimentPopoverContent e={e} />,
     }));
+
   return (
     <div>
       {sorted.map((e, i) => (
         <Fragment key={e.id}>
           <PhenopacketLink.Experiment packetId={packetId} experimentId={e.id} preserveQueryParams>
-            <Tooltip title={e.id} styles={{ body: { wordWrap: 'normal', inlineSize: 'max-content' } }}>
+            <Popover content={e.content}>
               {t(e.displayType)}
               {e.hasDuplicates ? ` (${e.typeIndex})` : ''}
-            </Tooltip>
+            </Popover>
           </PhenopacketLink.Experiment>
 
           {i < sorted.length - 1 ? ', ' : ''}
