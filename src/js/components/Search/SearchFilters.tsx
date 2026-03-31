1f7ba6ab676e9ef43665a9195b6836be17396342
David Lougheed <david.lougheed@gmail.com>
2026-03-31 10:17:44 -0400

    fix field change logic + a bit of code cleanup for filter change fn

--- src/js/components/Search/SearchFilters.tsx
+++ src/js/components/Search/SearchFilters.tsx
@@ -12,12 +12,15 @@ import {
   buildQueryParamsUrl,
   combineQueryParamsWithoutKey,
   filtersStateToQueryParamEntries,
-  queryParamsWithoutKey,
 } from '@/features/search/utils';
 
-import type { QueryParamEntries } from '@/features/search/types';
+import type { QueryParamEntries, QueryParamEntry } from '@/features/search/types';
 import SearchFilterInput, { type FilterInputValue, SearchFilterInputSkeleton } from './SearchFilterInput';
 import SearchSubForm, { type DefinedSearchSubFormProps } from '@/components/Search/SearchSubForm';
+
+const buildValueToEntry =
+  (f: string) =>
+  (v: string | null | undefined): QueryParamEntry => [f, v || ''];
 
 const SearchFilters = (props: DefinedSearchSubFormProps) => {
   const { pathname } = useLocation();
@@ -53,86 +56,96 @@ const SearchFilters = (props: DefinedSearchSubFormProps) => {
         {WAITING_STATES.includes(configStatus) || WAITING_STATES.includes(fieldsStatus) ? (
           <SearchFilterInputSkeleton />
         ) : (
-          filterInputs.map((fv, i) => (
-            <SearchFilterInput
-              key={i}
-              onChange={({ field, value }) => {
-                if (field === null) return; // Force field to resolve as string type
-                console.debug('[SearchFilters] SearchFilterInput onChange called; field =', field, 'value =', value);
+          filterInputs.map((fv, i) => {
+            const onChange = ({ field, value }: FilterInputValue) => {
+              if (field === null) return; // Force field to resolve as string type
+              console.debug('[SearchFilters] SearchFilterInput onChange called; field =', field, 'value =', value);
 
-                let existingFiltersQP = filtersStateToQueryParamEntries(filters, true);
-                let newEntries: QueryParamEntries = [];
+              let existingFiltersQP = filtersStateToQueryParamEntries(filters, true);
+              let newEntries: QueryParamEntries = [];
+              const toEntry = buildValueToEntry(field);
 
-                const oldValue = filters[field];
+              const oldValue = filters[field];
 
-                if (field in filters && Array.isArray(value)) {
-                  // same field, array length just changed - special case where we don't want to shift the order if
-                  // we're just changing the size of the values array.
-                  if (Array.isArray(oldValue)) {
-                    existingFiltersQP = existingFiltersQP.filter(
-                      ([k, v]) => k !== field || (k === field && value.includes(v))
-                    );
-                    // any new values in the array we still need to add to new entries:
-                    newEntries = value.filter((v) => !oldValue.includes(v)).map((v) => [field, v || '']);
-                  } else if (value.length > 0) {
-                    // oldValue is not an array, so we're either:
-                    //  - adding a new value to an existing value, making a multiple selection ...
-                    newEntries = value.filter((v) => oldValue !== v).map((v) => [field, v || '']);
-                  } else {
-                    //  - ... or removing the only value of the field (handled above)
-                    existingFiltersQP = existingFiltersQP.map(([k, v]) => [k, k === field ? '' : v]);
-                  }
-                } else if (field in filters && !Array.isArray(value)) {
-                  // If the field stays the same, we will put it back with a new value.
-                  existingFiltersQP = existingFiltersQP.map(([k, v]) => [k, k === field ? value || '' : v]);
-                } else {
-                  // If we change the field in this filter, we need to remove it ...
-                  existingFiltersQP = queryParamsWithoutKey(
-                    existingFiltersQP,
-                    fv.field && fv.field !== field ? fv.field : field
+              if (field in filters && Array.isArray(value)) {
+                // same field, array length just changed - special case where we don't want to shift the order if
+                // we're just changing the size of the values array.
+                if (Array.isArray(oldValue)) {
+                  existingFiltersQP = existingFiltersQP.filter(
+                    ([k, v]) => k !== field || (k === field && value.includes(v))
                   );
-                  // ... and put the new field in.
-                  newEntries = Array.isArray(value) ? value.map((v) => [field, v]) : [[field, value || '']];
+                  // any new values in the array we still need to add to new entries:
+                  newEntries = value.filter((v) => !oldValue.includes(v)).map(toEntry);
+                } else if (value.length > 0) {
+                  // oldValue is not an array, so we're either:
+                  //  - adding a new value to an existing value, making a multiple selection ...
+                  newEntries = value.filter((v) => oldValue !== v).map(toEntry);
+                } else {
+                  //  - ... or removing the only value of the field (handled above)
+                  existingFiltersQP = existingFiltersQP.map(([k, v]) => [k, k === field ? '' : v]);
                 }
+              } else if (field in filters && !Array.isArray(value)) {
+                // If the field stays the same, we will put it back with a new value.
+                existingFiltersQP = existingFiltersQP.map(([k, v]) => [k, k === field ? value || '' : v]);
+              } else if (fv.field) {
+                // If we change the field in this filter, swap it out with the first instance of the old field and
+                // remove other instances of the old field.
+                let replaced = false;
+                existingFiltersQP = existingFiltersQP
+                  .map<QueryParamEntry>(([k, v]) => {
+                    if (replaced || k !== fv.field) return [k, v];
+                    // replace with new field + first new value or blank:
+                    replaced = true;
+                    return toEntry(Array.isArray(value) ? value[0] : value);
+                  })
+                  .filter(([k, _]) => k !== fv.field);
+                // If we have multiple new values for our new field (currently not a case which happens), put them in
+                newEntries = Array.isArray(value) ? value.slice(1).map(toEntry) : [];
+              } else {
+                // We're adding a new filter field, so all we have to do is add new entries:
+                newEntries = Array.isArray(value) ? value.map(toEntry) : [toEntry(value)];
+              }
 
-                const url = buildQueryParamsUrl(pathname, [
-                  ...existingFiltersQP,
-                  ...newEntries,
-                  ...entityAndTextQueryParams,
-                  // If we have an entity table page set, we need to reset it to 0 if the filter changes:
-                  ...(entityAndTextQueryParams.find(([k, _]) => k === TABLE_PAGE_QUERY_PARAM)
-                    ? ([[TABLE_PAGE_QUERY_PARAM, '0']] as QueryParamEntries)
-                    : []),
-                ]);
-                console.debug('[SearchFilters] Redirecting to:', url);
-                navigate(url, { replace: true });
-                // Don't need to dispatch - the code handling the URL change will dispatch the fetch for us instead.
-              }}
-              onRemove={() => {
-                if (fv.field === null) return;
-                const filtersStateAsQueryParams = filtersStateToQueryParamEntries(filters, true);
-                const url = buildQueryParamsUrl(
-                  pathname,
-                  // Remove fv.field from query params + ensure no [TEXT_QUERY_PARAM] key.
-                  // Also, reset the page if we need to:
-                  combineQueryParamsWithoutKey(
-                    filtersStateAsQueryParams,
-                    [
-                      ...entityAndTextQueryParams,
-                      ...(entityAndTextQueryParams.find(([k, _]) => k === TABLE_PAGE_QUERY_PARAM)
-                        ? ([[TABLE_PAGE_QUERY_PARAM, '0']] as QueryParamEntries)
-                        : []),
-                    ],
-                    [fv.field]
-                  )
-                );
-                console.debug('[SearchFilters] Redirecting to:', url);
-                navigate(url, { replace: true });
-              }}
-              disabledFields={usedFields}
-              {...fv}
-            />
-          ))
+              const url = buildQueryParamsUrl(pathname, [
+                ...existingFiltersQP,
+                ...newEntries,
+                ...entityAndTextQueryParams,
+                // If we have an entity table page set, we need to reset it to 0 if the filter changes:
+                ...(entityAndTextQueryParams.find(([k, _]) => k === TABLE_PAGE_QUERY_PARAM)
+                  ? ([[TABLE_PAGE_QUERY_PARAM, '0']] as QueryParamEntries)
+                  : []),
+              ]);
+              console.debug('[SearchFilters] Redirecting to:', url);
+              navigate(url, { replace: true });
+              // Don't need to dispatch - the code handling the URL change will dispatch the fetch for us instead.
+            };
+
+            const onRemove = () => {
+              if (fv.field === null) return;
+              const filtersStateAsQueryParams = filtersStateToQueryParamEntries(filters, true);
+              const url = buildQueryParamsUrl(
+                pathname,
+                // Remove fv.field from query params + ensure no [TEXT_QUERY_PARAM] key.
+                // Also, reset the page if we need to:
+                combineQueryParamsWithoutKey(
+                  filtersStateAsQueryParams,
+                  [
+                    ...entityAndTextQueryParams,
+                    ...(entityAndTextQueryParams.find(([k, _]) => k === TABLE_PAGE_QUERY_PARAM)
+                      ? ([[TABLE_PAGE_QUERY_PARAM, '0']] as QueryParamEntries)
+                      : []),
+                  ],
+                  [fv.field]
+                )
+              );
+              console.debug('[SearchFilters] Redirecting to:', url);
+              navigate(url, { replace: true });
+            };
+
+            return (
+              <SearchFilterInput key={i} onChange={onChange} onRemove={onRemove} disabledFields={usedFields} {...fv} />
+            );
+          })
         )}
       </Space>
     </SearchSubForm>
