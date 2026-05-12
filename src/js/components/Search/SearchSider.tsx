82717929ff79ce602e70154f520e026fbf5dce80
David Lougheed <david.lougheed@gmail.com>
2026-05-12 10:23:54 -0400

    disable catalogue search for now

--- src/js/components/Search/SearchSider.tsx
+++ src/js/components/Search/SearchSider.tsx
@@ -1,12 +1,9 @@
 import clsx from 'clsx';
-import { Divider, Input, Layout, Typography } from 'antd';
-import { FormOutlined } from '@ant-design/icons';
+import { Divider, Layout } from 'antd';
 import SearchForm from '@/components/Search/SearchForm';
 import { useSelectedScope } from '@/features/metadata/hooks';
-import { useTranslationFn } from '@/hooks';
 
 const SearchSider = ({ collapsed, overlay }: { collapsed: boolean; overlay: boolean }) => {
-  const t = useTranslationFn();
   const { scope } = useSelectedScope();
   return (
     <Layout.Sider
@@ -17,20 +14,21 @@ const SearchSider = ({ collapsed, overlay }: { collapsed: boolean; overlay: bool
         minHeight: scope.project ? 'calc(100vh - var(--header-height) - var(--content-scoped-title-height))' : 0,
       }}
     >
-      {scope.project ? (
-        <div id="search-sider__inner" style={{ top: 'calc(var(--content-scoped-title-height) + 1px)' }}>
-          <SearchForm />
-          <Divider className="m-0" />
-        </div>
-      ) : (
-        <div id="search-sider__inner" style={{ top: 0, padding: 24, boxSizing: 'border-box' }}>
-          {/* TODO: Hook up catalogue search */}
-          <Typography.Title level={3} className="search-sub-form-title">
-            <FormOutlined /> {t('search.text_search')}
-          </Typography.Title>
-          <Input.Search />
-        </div>
-      )}
+      {
+        scope.project ? (
+          <div id="search-sider__inner" style={{ top: 'calc(var(--content-scoped-title-height) + 1px)' }}>
+            <SearchForm />
+            <Divider className="m-0" />
+          </div>
+        ) : null
+        // TODO: hook up catalogue search
+        // <div id="search-sider__inner" style={{ top: 0, padding: 24, boxSizing: 'border-box' }}>
+        //   <Typography.Title level={3} className="search-sub-form-title">
+        //     <FormOutlined /> {t('search.text_search')}
+        //   </Typography.Title>
+        //   <Input.Search />
+        // </div>
+      }
     </Layout.Sider>
   );
 };
