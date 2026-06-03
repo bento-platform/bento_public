176ca270fb14888878d3a7a7a1a8d3f3dee4e280
Gordon Krieger <gordon.krieger@gmail.com>
2026-06-03 10:43:42 -0400

    hook-free DRS access

--- src/js/features/drs/hooks.ts
+++ src/js/features/drs/hooks.ts
@@ -66,11 +66,73 @@ export const useDrsAccessMethods = (url: string | undefined): string | null => {
 
 
 
-// ------------------------------- async versions, not hooks
+// ------------------------------- async versions, not hooks. to move
 
 export const getDrsAccessMethods = async(url: string | undefined): Promise<string | null> => {
   return getDrsObjectOrPassThrough(url).then((record) => (record?.access_methods ?? []).find((am) => am.type === 'https')?.access_url?.url ?? null);
 };
+
+// export const getDrsObjectOrPassThrough = async (uri: string | undefined): Promise<DrsRecord | null> => {
+//   /**
+//    * If a record is returned, the URI is a DRS URI that has been fetched.
+//    * If null is returned, the URI is not recognized as a DRS URI.
+//    */
+
+
+//   if (!uri) return null;
+
+//   // deduplicate
+//   const isDrs = (() => {
+//     try {
+//       const parts = new URL(uri);
+//       return parts.protocol === 'drs:';
+//     } catch {
+//       return false;
+//     }
+//   })();
+
+//   if (!isDrs) return null;
+
+//   const currentState = store.getState();
+//   const recordState = currentState.drs.byUri[uri];
+
+//   if (recordState?.status === RequestStatus.Fulfilled) {
+//     return recordState.record ?? null;
+//   }
+
+//   if (recordState?.status === RequestStatus.Pending) {
+//     // if request is pending, don't launch a second dispatch, which will get aborted
+//     // instead subscribe to store updates and resolve when the request finishes
+//     return new Promise((resolve) => {
+//       const unsubscribe = store.subscribe(() => {
+//         const nextState = store.getState();
+//         const nextRecordState = nextState.drs.byUri[uri];
+
+//         if (!nextRecordState) return;
+//         if (
+//           nextRecordState.status === RequestStatus.Fulfilled ||
+//           nextRecordState.status === RequestStatus.Rejected
+//         ) {
+//           unsubscribe();
+//           resolve(nextRecordState.status === RequestStatus.Fulfilled ? nextRecordState.record : null);
+//         }
+//       });
+//     });
+//   }
+
+//   try {
+//     const record = await store.dispatch(getDrsRecord(uri)).unwrap();
+//     return record;
+//   } catch (err) {
+//     const finalState = store.getState().drs.byUri[uri];
+//     if (finalState?.status === RequestStatus.Fulfilled) {
+//       return finalState.record ?? null;
+//     }
+//     return null;
+//   }
+// };
+
+
 
 export const getDrsObjectOrPassThrough = async (uri: string | undefined): Promise<DrsRecord | null> => {
   /**
@@ -93,8 +155,41 @@ export const getDrsObjectOrPassThrough = async (uri: string | undefined): Promis
 
   if (!isDrs) return null;
 
-  // add try/catch
-  // direct call to store.dispatch instead of useAppDispatch hook
-  const record = store.dispatch(getDrsRecord(uri)).unwrap()
-  return record;
+  const currentState = store.getState();
+  const recordState = currentState.drs.byUri[uri];
+
+  if (recordState?.status === RequestStatus.Fulfilled) {
+    return recordState.record ?? null;
+  }
+
+  if (recordState?.status === RequestStatus.Pending) {
+    // if request is pending, don't launch a second dispatch, which will get aborted
+    // instead subscribe to store updates and resolve when the request finishes
+    return new Promise((resolve) => {
+      const unsubscribe = store.subscribe(() => {
+        const nextState = store.getState();
+        const nextRecordState = nextState.drs.byUri[uri];
+
+        if (!nextRecordState) return;
+        if (
+          nextRecordState.status === RequestStatus.Fulfilled ||
+          nextRecordState.status === RequestStatus.Rejected
+        ) {
+          resolve(nextRecordState.status === RequestStatus.Fulfilled ? nextRecordState.record : null);
+        }
+      });
+      unsubscribe()
+    });
+  }
+
+  try {
+    const record = await store.dispatch(getDrsRecord(uri)).unwrap();
+    return record;
+  } catch (err) {
+    const finalState = store.getState().drs.byUri[uri];
+    if (finalState?.status === RequestStatus.Fulfilled) {
+      return finalState.record ?? null;
+    }
+    return null;
+  }
 };
