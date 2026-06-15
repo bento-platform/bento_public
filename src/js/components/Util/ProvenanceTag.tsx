7edfa5ab3cb17e01f292ebae47ea4ba0ababb0ea
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-06-15 15:27:47 -0400

    style(provenance-tag): lint fix

--- src/js/components/Util/ProvenanceTag.tsx
+++ src/js/components/Util/ProvenanceTag.tsx
@@ -1,8 +1,6 @@
 import type { ReactNode } from 'react';
 import { Tag } from 'antd';
 
-const ProvenanceTag = ({ children }: { children: ReactNode }) => (
-  <Tag className="provenance-tag">{children}</Tag>
-);
+const ProvenanceTag = ({ children }: { children: ReactNode }) => <Tag className="provenance-tag">{children}</Tag>;
 
 export default ProvenanceTag;
