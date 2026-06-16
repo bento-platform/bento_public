1416dd17651576bed2e68cc0554ecf6b5184e1f9
David Lougheed <david.lougheed@gmail.com>
2026-06-16 16:32:34 -0400

    work on participant criteria colour-coded display

--- src/js/components/Provenance/ParticipantCriteriaDisplay.tsx
+++ src/js/components/Provenance/ParticipantCriteriaDisplay.tsx
@@ -1,11 +1,20 @@
-import type { ParticipantCriteria } from '@/types/dataset';
+import { Tag, type TagProps } from 'antd';
+import type { ParticipantCriteria, ParticipantCriterionType } from '@/types/dataset';
 import { useTranslationFn } from '@/hooks';
 import BaseProvenanceTable from '@/components/Provenance/Tables/BaseProvenanceTable';
+
+const ParticipantCriterionTypeTag = ({ type }: { type: ParticipantCriterionType }) => {
+  const t = useTranslationFn();
+  let color: TagProps['color'] = undefined;
+  if (type === 'Inclusion') color = 'green';
+  else if (type === 'Exclusion') color = 'red';
+  return <Tag color={color}>{t(type)}</Tag>;
+};
 
 const ParticipantCriteriaDisplay = ({ criteria }: { criteria: ParticipantCriteria[] }) => {
   const t = useTranslationFn();
   return (
-    <BaseProvenanceTable
+    <BaseProvenanceTable<ParticipantCriteria>
       dataSource={criteria}
       rowKey="description"
       columns={[
@@ -14,7 +23,7 @@ const ParticipantCriteriaDisplay = ({ criteria }: { criteria: ParticipantCriteri
           dataIndex: 'type',
           key: 'type',
           width: 120,
-          render: (type) => t(type),
+          render: (type: ParticipantCriterionType) => <ParticipantCriterionTypeTag type={type} />,
         },
         {
           title: t('Description'),
@@ -25,6 +34,7 @@ const ParticipantCriteriaDisplay = ({ criteria }: { criteria: ParticipantCriteri
           title: t('Link'),
           dataIndex: 'link',
           key: 'link',
+          isEmpty: (link) => !!link,
           render: (link) =>
             link ? (
               <a href={link} target="_blank" rel="noreferrer">
