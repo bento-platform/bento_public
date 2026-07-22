d3aa11a10145fd88e5f8a6aa80cc3599aa73ce3a
David Lougheed <david.lougheed@gmail.com>
2026-07-22 12:27:30 -0400

    style: simplify provenance card markup + fill vertically

--- src/js/components/Provenance/DatasetProvenance/DatasetProvenanceCard.tsx
+++ src/js/components/Provenance/DatasetProvenance/DatasetProvenanceCard.tsx
@@ -1,3 +1,4 @@
+import type { CSSProperties } from 'react';
 import { Card } from 'antd';
 import type { Dataset } from '@/types/dataset';
 import DatasetProvenance from './DatasetProvenance';
@@ -10,12 +11,19 @@ export type DatasetProvenanceProps = {
   hideHeader?: boolean;
 };
 
+const MIN_HEIGHT_CSS: CSSProperties = {
+  minHeight: 'calc(100vh - 210px - var(--scoped-title-height) - var(--header-height) - 2 * var(--content-padding-v))',
+};
+
 const DatasetProvenanceCard = ({ dataset, loading, hideHeader }: DatasetProvenanceProps) => (
-  <div className="container margin-auto">
-    <Card className="shadow rounded-xl overflow-hidden" loading={loading} styles={{ body: { padding: 0 } }}>
-      <DatasetProvenance dataset={dataset} hideHeader={hideHeader} mode="page" />
-    </Card>
-  </div>
+  <Card
+    className="container mx-auto shadow rounded-xl overflow-hidden"
+    loading={loading}
+    style={MIN_HEIGHT_CSS}
+    styles={{ body: { padding: 0 } }}
+  >
+    <DatasetProvenance dataset={dataset} hideHeader={hideHeader} mode="page" style={MIN_HEIGHT_CSS} />
+  </Card>
 );
 
 export default DatasetProvenanceCard;
