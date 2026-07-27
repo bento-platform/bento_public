9ff2d016c63d19139de4a456ebed616aa8a1f211
David Lougheed <david.lougheed@gmail.com>
2026-07-27 16:49:41 -0400

    fix: mount provenance root path

--- src/js/components/BentoAppRouter.tsx
+++ src/js/components/BentoAppRouter.tsx
@@ -194,6 +194,7 @@ const BentoAppRouter = () => {
         <Route path="/" element={<ScopedRoute />}>
           <Route index element={<PublicOverview />} />
           <Route path={BentoRoute.Overview} element={<PublicOverview />} />
+          <Route path={BentoRoute.Provenance} element={<ProvenancePage />} />
           <Route path={`${BentoRoute.Phenopackets}/:packetId/:tab?`} element={<PhenopacketView />} />
           {BentoRoute.Beacon && <Route path={BentoRoute.Beacon} element={<BeaconQueryUi />} />}
           {/* Beacon network is only available at the top level - scoping does not make sense for it. */}
