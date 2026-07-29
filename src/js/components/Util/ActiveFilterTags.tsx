bc4408106d8a949a21621642b31062133840a7a3
Jose Garcia <jose.m.garcia@mcgill.ca>
2026-07-29 16:26:41 -0400

    refactor(public): refactoring the filter tags

--- src/js/components/Util/ActiveFilterTags.tsx
+++ src/js/components/Util/ActiveFilterTags.tsx
@@ -1,6 +1,7 @@
-import { Flex, Tag } from 'antd';
+import { Flex } from 'antd';
 import clsx from 'clsx';
 import { useTranslationFn } from '@/hooks';
+import { CloseOutlined } from '@ant-design/icons';
 
 export interface ActiveFilterPill {
   key: string;
@@ -22,14 +23,19 @@ const ActiveFilterTags = ({ pills, onClearAll, tagClassName }: ActiveFilterTagsP
   return (
     <Flex wrap gap={4} align="center">
       {pills.map(({ key, facetLabel, label, onClose }) => (
-        <Tag key={key} closable onClose={onClose} className={clsx('catalogue-filter-tag', tagClassName)}>
+        <div className={ clsx('catalogue-filter-tag', tagClassName)}>
           <span className="catalogue-filter-tag__facet-label">{t(facetLabel)}:</span>
-          {label}
-        </Tag>
+          <p className="catalogue-filter-tag__label">
+            {label}
+          </p>
+          <button aria-label={`${t('catalogue.toolbar.clear_filter')} ${t(facetLabel)} ${label}`} data-id={key} type="button" onClick={onClose} className="catalogue-filter-button focus-ring" >
+            <CloseOutlined aria-hidden="true" />
+          </button>
+        </div>
       ))}
-      <Tag className={clsx('catalogue-clear-tag', tagClassName)} onClick={onClearAll}>
+      <button type="button" className={clsx('catalogue-clear-tag focus-ring', tagClassName)} onClick={onClearAll}>
         {t('catalogue.toolbar.clear_all')}
-      </Tag>
+      </button>
     </Flex>
   );
 };
