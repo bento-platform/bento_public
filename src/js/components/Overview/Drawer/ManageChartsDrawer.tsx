548e8c260d1e719ec13ac3e8d622abf41a22fb02
David Lougheed <david.lougheed@gmail.com>
2026-08-11 16:09:17 -0400

    style: use grid for manage charts drawer

--- src/js/components/Overview/Drawer/ManageChartsDrawer.tsx
+++ src/js/components/Overview/Drawer/ManageChartsDrawer.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, type DrawerProps, Drawer, Flex, Grid, Space, Typography } from 'antd';
+import { Button, type DrawerProps, Drawer, Flex, Grid, Space, Typography } from 'antd';
 
 const { Title } = Typography;
 const { useBreakpoint } = Grid;
@@ -35,6 +35,7 @@ const ManageChartsDrawer = ({ onManageDrawerClose, manageDrawerVisible }: Manage
       // If we're on a small device, make the drawer full-screen width instead of a fixed width.
       // The default value for Ant Design is 372.
       width={isSmallScreen ? '100vw' : 420}
+      styles={{ body: { padding: 0, display: 'grid', gridTemplateRows: 'auto min-content' } }}
       extra={
         <Space>
           <Button
@@ -56,36 +57,44 @@ const ManageChartsDrawer = ({ onManageDrawerClose, manageDrawerVisible }: Manage
         </Space>
       }
     >
-      <OverviewChartSizeControl />
-      <Divider />
-      {sections.map(({ sectionTitle, charts }: { sectionTitle: string; charts: ChartDataField[] }, i: number) => (
-        <div key={i}>
-          <Flex justify="space-between" align="center" className={i === 0 ? 'mb-3' : 'my-3'}>
-            <Title level={5} style={{ margin: '0' }}>
-              {t(sectionTitle)}
-            </Title>
-            <Space>
-              <Button
-                size="small"
-                onClick={() => {
-                  dispatch(setAllDisplayedCharts({ section: sectionTitle }));
-                }}
-              >
-                {t('Show All')}
-              </Button>
-              <Button
-                size="small"
-                onClick={() => {
-                  dispatch(hideAllSectionCharts({ section: sectionTitle }));
-                }}
-              >
-                {t('Hide All')}
-              </Button>
-            </Space>
-          </Flex>
-          <ChartTree charts={charts} section={sectionTitle} />
-        </div>
-      ))}
+      <div className="p-ant-lg overflow-y-auto">
+        {sections.map(({ sectionTitle, charts }: { sectionTitle: string; charts: ChartDataField[] }, i: number) => (
+          <div key={i}>
+            <Flex justify="space-between" align="center" className={i === 0 ? 'mb-3' : 'my-3'}>
+              <Title level={5} style={{ margin: '0' }}>
+                {t(sectionTitle)}
+              </Title>
+              <Space>
+                <Button
+                  size="small"
+                  onClick={() => {
+                    dispatch(setAllDisplayedCharts({ section: sectionTitle }));
+                  }}
+                >
+                  {t('Show All')}
+                </Button>
+                <Button
+                  size="small"
+                  onClick={() => {
+                    dispatch(hideAllSectionCharts({ section: sectionTitle }));
+                  }}
+                >
+                  {t('Hide All')}
+                </Button>
+              </Space>
+            </Flex>
+            <ChartTree charts={charts} section={sectionTitle} />
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          padding: 'var(--ant-padding) var(--ant-padding-lg) var(--ant-padding-lg) var(--ant-padding-lg)',
+          borderTop: '1px solid var(--ant-color-split)',
+        }}
+      >
+        <OverviewChartSizeControl />
+      </div>
     </Drawer>
   );
 };
