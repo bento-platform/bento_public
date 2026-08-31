52622439e900dbce06045e9a17174644ecc58fb4
Jose Miguel <josepplloo@gmail.com>
2026-08-31 14:23:21 -0400

    bugfix: add visually-hidden class for the filters chips

--- src/js/components/Util/FilterChip.tsx
+++ src/js/components/Util/FilterChip.tsx
@@ -20,7 +20,7 @@ const FilterChip = ({ label, count, selected, disabled, onChange }: FilterChipPr
   return (
     <label htmlFor={id} className={clsx('fchip', selected && 'fchip--on')} title={labelTitle}>
       <input
-        className="fchip__input"
+        className="fchip__input visually-hidden"
         id={id}
         type="checkbox"
         checked={selected}
