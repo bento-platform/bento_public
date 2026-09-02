5d587a34b5a21a49d891776fb4a27eb7fc6e9b10
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-09-02 13:37:02 -0400

    chore(public): fix prettier lint error in dynamic App import comment

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-// ssr: false because the legacy app relies on browser-only APIs 
+// ssr: false because the legacy app relies on browser-only APIs
 // (Leaflet, antd's cssinjs, react-router-dom's BrowserRouter).
 // TODO: Adapt paths to static routes
 const App = dynamic(() => import('@/App'), { ssr: false });
