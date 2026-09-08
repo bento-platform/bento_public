47e858dedf80d786944db9e7f5e5e4a47008a2bf
Jose Miguel <josepplloo@gmail.com>
2026-09-08 16:57:08 -0400

    feature(public): pgcl footer demo data

--- src/js/components/Pcgl/PcglFooter.tsx
+++ src/js/components/Pcgl/PcglFooter.tsx
@@ -1,11 +1,89 @@
-import { Flex, Grid, Layout } from 'antd';
+import { Flex, Grid, Layout, Typography } from 'antd';
 import { useTranslationFn } from '@/hooks';
 import clsx from 'clsx';
-
-import { ADMIN_URL, SHOW_ADMIN_LINK } from '@/config';
+import { ADMIN_URL, SHOW_ADMIN_LINK, PCGL_MODE } from '@/config';
+import { useId } from 'react';
 
 const { Footer } = Layout;
 const { useBreakpoint } = Grid;
+const { Title } = Typography;
+
+type FooterNavItem = { link: string; url: string };
+type FooterNavItems = { title: string; items: FooterNavItem[] };
+
+const LinkHeader = ({ children }: { children: React.ReactNode }) => <Title level={3}>{children}</Title>;
+
+const LinkItem = ({ link, url }: { link: string; url: string }) => (
+  <a className="focus-ring" href={url} rel="noreferrer" target="_blank">
+    {link}
+  </a>
+);
+
+const FooterView = ({ sections }: { sections: FooterNavItems[] }) => {
+  const key = useId();
+  return (
+    <nav>
+      {sections.map((section) => (
+        <div key={section.title}>
+          <LinkHeader>{section.title}</LinkHeader>
+          <ul>
+            {section.items.map((item) => (
+              <li key={key}>
+                <LinkItem link={item.link} url={item.url} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+const FooterContainer = () => {
+  //TODO: FOOTER_DATA: Could we get it from an API?
+  const FOOTER_DATA = [
+    {
+      title: 'about',
+      items: [
+        { link: 'pcglWebsite', url: 'pcglWebsite' },
+        { link: 'policies', url: 'policies' },
+        { link: 'privacy', url: 'privacy' },
+        { link: 'termsConditions', url: 'termsConditions' },
+        { link: 'publicationPolicy', url: 'publicationPolicy' },
+      ],
+    },
+    {
+      title: 'resources',
+      items: [
+        { link: 'helpGuides', url: 'helpGuides' },
+        { link: 'controlledDataUsers', url: 'controlledDataUsers' },
+        { link: 'dataPlatform', url: 'dataPlatform' },
+        { link: 'researchPlatform', url: 'researchPlatform' },
+      ],
+    },
+    {
+      title: 'connect',
+      items: [
+        { link: 'contact', url: 'contact' },
+        { link: 'subscribe', url: 'subscribe' },
+        { link: 'linkedin', url: 'linkedin' },
+        { link: 'github', url: 'github' },
+      ],
+    },
+  ];
+
+  const t = useTranslationFn();
+
+  const data = Array.from(FOOTER_DATA, (link) => ({
+    title: t(`pcgl.footer.sections.${link.title}`),
+    items: link.items.map((item) => ({
+      link: t(`pcgl.footer.urls.${item.link}`),
+      url: t(`pcgl.footer.links.${item.url}`),
+    })),
+  }));
+
+  return <FooterView sections={data} />;
+};
 
 const PCGL_LINKS: { key: string; href?: string }[] = [
   {
