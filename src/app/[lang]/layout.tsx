18158103d29d6d268c1ad079afc42f8a3db0f9ee
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-09-10 14:09:48 -0400

    feat: added slug for lang and provider for i18n

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,31 @@
+import { initServerI18next, getT, getResources, generateI18nStaticParams } from 'next-i18next/server'
+import { I18nProvider } from 'next-i18next/client'
+import i18nConfig from '../../i18n.config'
+
+initServerI18next(i18nConfig)
+
+export async function generateStaticParams() {
+  return generateI18nStaticParams()
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ lng: string }>
+}) {
+  const { lng } = await params
+  const { i18n } = await getT()
+  const resources = getResources(i18n)
+
+  return (
+    <html lang={lng}>
+      <body>
+        <I18nProvider language={lng} resources={resources}>
+          {children}
+        </I18nProvider>
+      </body>
+    </html>
+  )
+}
