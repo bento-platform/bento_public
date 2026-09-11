de6acf3e571b31739c40d5e42f9bb538415c70bc
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-09-11 10:33:42 -0400

    fix: merge root layout into [lang] to fix nested html/script errors

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from 'next'
+import Script from 'next/script'
 import { initServerI18next, getT, getResources, generateI18nStaticParams } from 'next-i18next/server'
 import { I18nProvider } from 'next-i18next/client'
 import i18nConfig from '../../i18n.config'
 
 initServerI18next(i18nConfig)
+
+export const metadata: Metadata = {
+  title: 'Bento',
+}
 
 export async function generateStaticParams() {
   return generateI18nStaticParams()
@@ -21,7 +27,18 @@ export default async function RootLayout({
 
   return (
     <html lang={lang}>
+      <head>
+        {/* Overridable instance CSS file for custom styling */}
+        {/* eslint-disable-next-line @next/next/no-css-tags */}
+        <link rel="stylesheet" href="/public/styles/instance.css" />
+      </head>
       <body>
+        <Script
+          id="bento-public-config-fallback"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: 'let BENTO_PUBLIC_CONFIG = {};' }}
+        />
+        <Script src="/public/config.js" strategy="beforeInteractive" />
         <I18nProvider
           language={lang}
           resources={resources}
