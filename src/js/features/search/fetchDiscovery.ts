9c99ef6555ef9e08dc53b076acd1526cc2bf064c
David Lougheed <david.lougheed@gmail.com>
2026-09-18 11:22:21 -0400

    address review comment

--- src/js/features/search/fetchDiscovery.ts
+++ src/js/features/search/fetchDiscovery.ts
@@ -5,5 +5,5 @@ import type { QueryParamEntries } from '@/utils/queryParams';
 import { katsuDiscoveryUrl } from '@/constants/configConstants';
 import { scopedAuthorizedRequestConfig } from '@/utils/requests';
 
-export const getDiscovery = async (state: RootState, queryParams?: QueryParamEntries) =>
+export const fetchDiscovery = async (state: RootState, queryParams?: QueryParamEntries) =>
   await axios.get(katsuDiscoveryUrl, scopedAuthorizedRequestConfig(state, queryParams)).then((res) => res.data);
