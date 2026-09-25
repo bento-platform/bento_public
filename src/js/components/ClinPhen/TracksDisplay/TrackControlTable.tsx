1521569e1d0c5b2a798e53157de30eaf5bd06333
Gordon Krieger <gordon.krieger@gmail.com>
2026-09-25 13:56:14 -0400

    tracks: memoize tracks in track controls

--- src/js/components/ClinPhen/TracksDisplay/TrackControlTable.tsx
+++ src/js/components/ClinPhen/TracksDisplay/TrackControlTable.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Switch, Table } from 'antd';
 import type { ExperimentResultWithView } from '@/types/clinPhen/igv';
 import { useTranslationFn } from '@/hooks';
@@ -11,35 +12,38 @@ const TrackControlTable = ({
   tracks: ExperimentResultWithView[];
 }) => {
   const t = useTranslationFn();
-  const trackTableColumns = [
-    {
-      title: t('entities.experiment_result_one'),
-      dataIndex: 'filename',
-    },
-    {
-      title: t('experiment_result.description'),
-      dataIndex: 'description',
-    },
-    {
-      title: t('experiment_result.file_format'),
-      dataIndex: 'file_format',
-    },
-    {
-      title: t('experiment_result.genome_assembly_id'),
-      dataIndex: 'genome_assembly_id',
-      render: (_: unknown, track: ExperimentResultWithView) => (
-        <ReferenceGenomePopoverField referenceGenomeId={track.genome_assembly_id} />
-      ),
-    },
-    {
-      title: t('general.view'),
-      key: 'view',
-      align: 'center' as const,
-      render: (_: unknown, track: ExperimentResultWithView) => (
-        <Switch checked={track.viewInIgv} onChange={() => toggleView(track)} />
-      ),
-    },
-  ];
+  const trackTableColumns = useMemo(
+    () => [
+      {
+        title: t('entities.experiment_result_one'),
+        dataIndex: 'filename',
+      },
+      {
+        title: t('experiment_result.description'),
+        dataIndex: 'description',
+      },
+      {
+        title: t('experiment_result.file_format'),
+        dataIndex: 'file_format',
+      },
+      {
+        title: t('experiment_result.genome_assembly_id'),
+        dataIndex: 'genome_assembly_id',
+        render: (_: unknown, track: ExperimentResultWithView) => (
+          <ReferenceGenomePopoverField referenceGenomeId={track.genome_assembly_id} />
+        ),
+      },
+      {
+        title: t('general.view'),
+        key: 'view',
+        align: 'center' as const,
+        render: (_: unknown, track: ExperimentResultWithView) => (
+          <Switch checked={track.viewInIgv} onChange={() => toggleView(track)} />
+        ),
+      },
+    ],
+    [t, toggleView]
+  );
 
   return (
     <Table
